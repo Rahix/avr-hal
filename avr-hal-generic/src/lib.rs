24043e2b86681f509628857ee31d7a07bc4e1555
Rahix <rahix@rahix.de>
2019-05-23 19:45:31 +0100

    Add preludes
    Signed-off-by: Rahix <rahix@rahix.de>

--- avr-hal-generic/src/lib.rs
+++ avr-hal-generic/src/lib.rs
@@ -8,3 +8,13 @@ pub extern crate void;
 pub mod clock;
 pub mod delay;
 pub mod port;
+
+pub mod prelude {
+    pub use hal::prelude::*;
+    pub use hal::digital::v2::OutputPin as _embedded_hal_digital_v2_OutputPin;
+    pub use hal::digital::v2::InputPin as _embedded_hal_digital_v2_InputPin;
+    pub use hal::digital::v2::StatefulOutputPin as _embedded_hal_digital_v2_StatefulOutputPin;
+    pub use hal::digital::v2::ToggleableOutputPin as _embedded_hal_digital_v2_ToggleableOutputPin;
+    pub use void::ResultVoidExt as _void_ResultVoidExt;
+    pub use void::ResultVoidErrExt as _void_ResultVoidErrExt;
+}
--- chips/atmega32u4-hal/src/lib.rs
+++ chips/atmega32u4-hal/src/lib.rs
@@ -1,3 +1,5 @@
+#![no_std]
+
 extern crate avr_hal_generic as avr_hal;
 pub extern crate avr_device as atmega32u4;
 
@@ -5,3 +7,8 @@ pub use avr_hal::clock;
 pub use avr_hal::delay;
 
 pub mod port;
+
+pub mod prelude {
+    pub use crate::avr_hal::prelude::*;
+    pub use crate::port::PortExt as _atmega_PortExt;
+}
