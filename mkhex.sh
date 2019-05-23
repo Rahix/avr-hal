6b4b8a392b68c9ebaa569f55081ecd6019e793d8
Rahix <rahix@rahix.de>
2019-05-23 20:00:41 +0100

    Add mkhex script
    Signed-off-by: Rahix <rahix@rahix.de>

---
+++ mkhex.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+if [ $# -ne 1 ]; then
+    echo "usage: $0 <elf-name>"
+    exit 1
+fi
+
+# Go to the project root
+cd "$(dirname "$0")"
+
+ELF="target/avr-atmega32u4/release/examples/$1.elf"
+
+set -xe
+
+avr-objcopy -S -j .text -j .data -O ihex "$ELF" "target/$1.hex"
+
+set +x
+
+BYTES=$(avr-size "$ELF" | tail -1 | cut -f4 | bc)
+echo "$(numfmt --to=si "$BYTES") Bytes used ($BYTES exact)."
