af62d7e90aad0ec971bc635b14431a8db980c822
Rahix <rahix@rahix.de>
2019-05-24 20:27:02 +0100

    attiny85: Add target & fix library
    Signed-off-by: Rahix <rahix@rahix.de>

--- chips/attiny85-hal/src/lib.rs
+++ chips/attiny85-hal/src/lib.rs
@@ -1,3 +1,5 @@
+#![no_std]
+
 extern crate avr_hal_generic as avr_hal;
 pub extern crate avr_device as attiny85;
 
@@ -5,3 +7,8 @@ pub use avr_hal::clock;
 pub use avr_hal::delay;
 
 pub mod port;
+
+pub mod prelude {
+    pub use crate::avr_hal::prelude::*;
+    pub use crate::port::PortExt as _attiny_PortExt;
+}
