7f1d3907ce3b962b82258b8463dd75e30f953c6e
Rahix <rahix@rahix.de>
2019-06-09 11:31:32 +0200

    avr-hal-generic: Replace core::fmt with ufmt
    Signed-off-by: Rahix <rahix@rahix.de>

--- avr-hal-generic/src/lib.rs
+++ avr-hal-generic/src/lib.rs
@@ -7,6 +7,8 @@ pub extern crate embedded_hal as hal;
 pub extern crate nb;
 #[doc(hidden)]
 pub extern crate void;
+#[doc(hidden)]
+pub extern crate ufmt;
 
 pub mod clock;
 pub mod delay;
@@ -21,5 +23,5 @@ pub mod prelude {
     pub use hal::digital::v2::ToggleableOutputPin as _embedded_hal_digital_v2_ToggleableOutputPin;
     pub use void::ResultVoidExt as _void_ResultVoidExt;
     pub use void::ResultVoidErrExt as _void_ResultVoidErrExt;
-    pub use core::fmt::Write as _core_fmt_Write;
+    pub use ufmt::uWrite as _ufmt_uWrite;
 }
