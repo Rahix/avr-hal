9c23a71429d80193b7088e2791c2b407d898ad42
Gabriel Pickl <peacemotion@gmail.com>
2019-06-09 12:55:08 +0200

    Fixed pinouts.

--- chips/atmega1280-hal/src/port.rs
+++ chips/atmega1280-hal/src/port.rs
@@ -76,6 +76,12 @@ avr_hal::impl_port! {
 
         impl PortExt for atmega1280::PORTC {
             regs: (pinc, ddrc, portc),
+            pc0: (PC0, 0),
+            pc1: (PC1, 1),
+            pc2: (PC2, 2),
+            pc3: (PC3, 3),
+            pc4: (PC4, 4),
+            pc5: (PC5, 5),
             pc6: (PC6, 6),
             pc7: (PC7, 7),
         }
@@ -114,8 +120,14 @@ avr_hal::impl_port! {
 
         impl PortExt for atmega1280::PORTE {
             regs: (pine, ddre, porte),
+            pe0: (PE0, 0),
+            pe1: (PE1, 1),
             pe2: (PE2, 2),
+            pe3: (PE3, 3),
+            pe4: (PE4, 4),
+            pe5: (PE5, 5),
             pe6: (PE6, 6),
+            pe7: (PE7, 7),
         }
     }
 }
@@ -132,6 +144,8 @@ avr_hal::impl_port! {
             regs: (pinf, ddrf, portf),
             pf0: (PF0, 0),
             pf1: (PF1, 1),
+            pf2: (PF2, 2),
+            pf3: (PF3, 3),
             pf4: (PF4, 4),
             pf5: (PF5, 5),
             pf6: (PF6, 6),
@@ -152,10 +166,10 @@ avr_hal::impl_port! {
             regs: (ping, ddrg, portg),
             pg0: (PG0, 0),
             pg1: (PG1, 1),
+            pg2: (PG2, 2),
+            pg3: (PG3, 3),
             pg4: (PG4, 4),
             pg5: (PG5, 5),
-            pg6: (PG6, 6),
-            pg7: (PG7, 7),
         }
     }
 }
@@ -172,6 +186,8 @@ avr_hal::impl_port! {
             regs: (pinh, ddrh, porth),
             ph0: (PH0, 0),
             ph1: (PH1, 1),
+            ph2: (PH2, 2),
+            ph3: (PH3, 3),
             ph4: (PH4, 4),
             ph5: (PH5, 5),
             ph6: (PH6, 6),
@@ -192,6 +208,8 @@ avr_hal::impl_port! {
             regs: (pinj, ddrj, portj),
             pj0: (PJ0, 0),
             pj1: (PJ1, 1),
+            pj2: (PJ2, 2),
+            pj3: (PJ3, 3),
             pj4: (PJ4, 4),
             pj5: (PJ5, 5),
             pj6: (PJ6, 6),
@@ -212,6 +230,8 @@ avr_hal::impl_port! {
             regs: (pink, ddrk, portk),
             pk0: (PK0, 0),
             pk1: (PK1, 1),
+            pk2: (PK2, 2),
+            pk3: (PK3, 3),
             pk4: (PK4, 4),
             pk5: (PK5, 5),
             pk6: (PK6, 6),
@@ -232,6 +252,8 @@ avr_hal::impl_port! {
             regs: (pinl, ddrl, portl),
             pl0: (Pl0, 0),
             pl1: (Pl1, 1),
+            pl2: (Pl2, 2),
+            pl3: (Pl3, 3),
             pl4: (Pl4, 4),
             pl5: (Pl5, 5),
             pl6: (Pl6, 6),
