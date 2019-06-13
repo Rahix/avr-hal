ddba13de8bfab85284274a0320f0545949803e0a
Gabriel Pickl <peacemotion@gmail.com>
2019-06-13 18:50:12 +0200

    Added i2c bindings to atmega1280.

--- boards/bigavr6/src/lib.rs
+++ boards/bigavr6/src/lib.rs
@@ -8,3 +8,4 @@ pub use atmega1280_hal::prelude;
 
 pub type Delay = hal::delay::Delay<hal::clock::MHz16>;
 pub type Serial<IMODE> = atmega1280_hal::usart::Usart0<hal::clock::MHz16, IMODE>;
+pub type I2c<M> = hal::i2c::I2c<hal::clock::MHz16, M>;
--- chips/atmega1280-hal/src/lib.rs
+++ chips/atmega1280-hal/src/lib.rs
@@ -13,3 +13,33 @@ pub mod prelude {
     pub use crate::avr_hal::prelude::*;
     pub use crate::port::PortExt as _atmega_PortExt;
 }
+
+pub mod i2c {
+    use crate::port::portd;
+    pub use avr_hal::i2c::*;
+
+    avr_hal::impl_twi_i2c! {
+        pub struct I2c {
+            peripheral: crate::atmega1280::TWI,
+            pins: {
+                sda: portd::PD1,
+                scl: portd::PD0,
+            },
+            registers: {
+                control: twcr {
+                    enable: twen,
+                    ack: twea,
+                    int: twint,
+                    start: twsta,
+                    stop: twsto,
+                },
+                status: twsr {
+                    prescaler: twps,
+                    status: tws,
+                },
+                bitrate: twbr,
+                data: twdr,
+            },
+        }
+    }
+}
