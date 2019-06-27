b2172a1bfc7e40d2d49a92f74b7d92d1f946c4cf
Jonah Dahlquist <jonah@jonah.name>
2019-06-26 23:56:01 -0500

    Ported TWI example from Leonardo to Uno

---
+++ boards/arduino-uno/examples/uno-i2cdetect.rs
@@ -0,0 +1,39 @@
+#![no_std]
+#![no_main]
+#![feature(proc_macro_hygiene)]
+
+extern crate panic_halt;
+use arduino_uno::prelude::*;
+
+#[no_mangle]
+pub extern fn main() -> ! {
+    let dp = arduino_uno::Peripherals::take().unwrap();
+
+    let mut delay = arduino_uno::Delay::new();
+    let mut pins = arduino_uno::Pins::new(
+        dp.PORTB,
+        dp.PORTC,
+        dp.PORTD,
+    );
+    let mut serial = arduino_uno::Serial::new(
+        dp.USART0,
+        pins.d0,
+        pins.d1.into_output(&mut pins.ddr),
+        57600,
+    );
+    let mut i2c = arduino_uno::I2c::new(
+        dp.TWI,
+        pins.a4.into_pull_up_input(&mut pins.ddr),
+        pins.a5.into_pull_up_input(&mut pins.ddr),
+        50000,
+    );
+
+    ufmt::uwriteln!(&mut serial, "Write direction test:\r").unwrap();
+    i2c.i2cdetect(&mut serial, arduino_uno::hal::i2c::Direction::Write).unwrap();
+    ufmt::uwriteln!(&mut serial, "\r\nRead direction test:\r").unwrap();
+    i2c.i2cdetect(&mut serial, arduino_uno::hal::i2c::Direction::Read).unwrap();
+
+    loop {
+        delay.delay_ms(1000);
+    }
+}
