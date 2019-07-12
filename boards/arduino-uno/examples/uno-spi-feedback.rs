d2b922fe66150febfb56716992954f1b78847ab3
Jonah Dahlquist <jonah@jonah.name>
2019-07-12 09:16:58 -0500

    Added comments to SPI example

--- boards/arduino-uno/examples/uno-spi-feedback.rs
+++ boards/arduino-uno/examples/uno-spi-feedback.rs
@@ -1,3 +1,15 @@
+//! This example demonstrates how to set up a SPI interface and communicate over it.
+//! The physical hardware configuation consists of connecting a jumper directly from pin `~11` to
+//! pin `~12`.
+//!
+//! Once this program is written to the board, the serial output can be accessed with
+//!
+//! ```
+//! sudo screen /tty/ACM0 57600
+//! ```
+//!
+//! You should see it output the line `data: 15` repeatedly (aka 0b00001111)
+
 #![no_std]
 #![no_main]
 #![feature(proc_macro_hygiene)]
@@ -13,6 +25,7 @@ pub extern fn main() -> ! {
         dp.PORTC,
         dp.PORTD,
     );
+    // set up serial interface for text output
     let mut serial = arduino_uno::Serial::new(
         dp.USART0,
         pins.d0,
@@ -21,6 +34,7 @@ pub extern fn main() -> ! {
     );
 
     pins.d10.into_output(&mut pins.ddr);// SS must be set to output mode
+    // create SPI interface
     let mut spi = Spi::new(
         dp.SPI,
         pins.d11.into_output(&mut pins.ddr),
@@ -29,7 +43,9 @@ pub extern fn main() -> ! {
     );
 
     loop {
+        // Send a byte
         spi.send(0b00001111).unwrap();
+        // Because PISO is connected to POSI, the read data should be the same
         let data = spi.read().unwrap();
 
         ufmt::uwriteln!(&mut serial, "data: {}\r", data).unwrap();
