0b8fb34834b8f58a3c2a1ae260f43eda81a968aa
Rahix <rahix@rahix.de>
2020-07-26 20:17:30 +0200

    atmega32u4: Implement ADC peripheral
    Add basic implementation of ADC peripheral.  This version only includes
    support for _Single Ended Input_ measurements on ADC0 - ADC13.  No
    differential measurements yet.
    
    Signed-off-by: Rahix <rahix@rahix.de>

--- boards/arduino-leonardo/src/lib.rs
+++ boards/arduino-leonardo/src/lib.rs
@@ -11,6 +11,7 @@ pub use atmega32u4_hal::atmega32u4;
 pub use crate::atmega32u4::Peripherals;
 pub use atmega32u4_hal::prelude;
 pub use atmega32u4_hal::spi;
+pub use atmega32u4_hal::adc;
 pub use crate::pins::*;
 
 pub type Delay = hal::delay::Delay<hal::clock::MHz16>;
---
+++ chips/atmega32u4-hal/src/adc.rs
@@ -0,0 +1,50 @@
+use crate::port::portb::{PB4, PB5, PB6};
+use crate::port::portd::{PD4, PD6, PD7};
+use crate::port::portf::{PF0, PF1, PF4, PF5, PF6, PF7};
+
+pub use crate::avr_hal::adc::*;
+
+#[doc(hidden)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(u8)]
+pub enum AdcMux {
+    Adc0 = 0b000000,
+    Adc1 = 0b000001,
+    Adc4 = 0b000100,
+    Adc5 = 0b000101,
+    Adc6 = 0b000110,
+    Adc7 = 0b000111,
+
+    Adc8 = 0b100000,
+    Adc9 = 0b100001,
+    Adc10 = 0b100010,
+    Adc11 = 0b100011,
+    Adc12 = 0b100100,
+    Adc13 = 0b100101,
+}
+
+avr_hal::impl_adc! {
+    pub struct Adc {
+        type ChannelID = AdcMux;
+        peripheral: crate::atmega32u4::ADC,
+        set_mux: |peripheral, id| {
+            let id = id as u8;
+            peripheral.admux.modify(|_, w| w.mux().bits(id & 0x1f));
+            peripheral.adcsrb.modify(|_, w| w.mux5().bit(id & 0x20 != 0));
+        },
+        pins: {
+            pf0: (PF0, AdcMux::Adc0, didr0::adc0d),
+            pf1: (PF1, AdcMux::Adc1, didr0::adc1d),
+            pf4: (PF4, AdcMux::Adc4, didr0::adc4d),
+            pf5: (PF5, AdcMux::Adc5, didr0::adc5d),
+            pf6: (PF6, AdcMux::Adc6, didr0::adc6d),
+            pf7: (PF7, AdcMux::Adc7, didr0::adc7d),
+            pd4: (PD4, AdcMux::Adc8, didr2::adc8d),
+            pd6: (PD6, AdcMux::Adc9, didr2::adc9d),
+            pd7: (PD7, AdcMux::Adc10, didr2::adc10d),
+            pb4: (PB4, AdcMux::Adc11, didr2::adc11d),
+            pb5: (PB5, AdcMux::Adc12, didr2::adc12d),
+            pb6: (PB6, AdcMux::Adc13, didr2::adc13d),
+        }
+    }
+}
