c3903eaa59d3445c2d8ba6ef9fb5d42b0de446c1
Rahix <rahix@rahix.de>
2020-08-09 16:38:29 +0200

    arduino-uno: Properly document PWM module
    Instead of 'inheriting' the documentation from atmega328p-hal, add more
    appropriate docs which use the `Pins` struct instead of raw PXi names.
    
    Signed-off-by: Rahix <rahix@rahix.de>

--- boards/arduino-uno/src/lib.rs
+++ boards/arduino-uno/src/lib.rs
@@ -12,7 +12,39 @@ pub use crate::atmega328p::Peripherals;
 pub use atmega328p_hal::prelude;
 pub use atmega328p_hal::spi;
 pub use atmega328p_hal::adc;
-pub use atmega328p_hal::pwm;
+
+pub mod pwm {
+    //! Support for PWM pins
+    //!
+    //! The 3 timers of ATmega328P can be used for PWM on certain pins.
+    //! The PWM methods are from `embedded_hal::PwmPin`.
+    //!
+    //! # Example
+    //! ```
+    //! let mut pins = arduino_uno::Pins::new(dp.PORTB, dp.PORTC, dp.PORTD);
+    //!
+    //! let mut timer1 = arduino_uno::pwm::Timer1Pwm::new(dp.TC1);
+    //!
+    //! let mut pin = pins.d9.into_output(&mut pins.ddr).into_pwm(&mut timer1);
+    //!
+    //! pin.set_duty(128);
+    //! pin.enable();
+    //! ```
+    //!
+    //! Here is an overview of pins and which timer they work with:
+    //!
+    //! | Pin | Conversion Method |
+    //! | --- | --- |
+    //! | `pins.d3` | `.into_pwm(&mut timer2)` |
+    //! | `pins.d5` | `.into_pwm(&mut timer0)` |
+    //! | `pins.d6` | `.into_pwm(&mut timer0)` |
+    //! | `pins.d9` | `.into_pwm(&mut timer1)` |
+    //! | `pins.d10` | `.into_pwm(&mut timer1)` |
+    //! | `pins.d11` | `.into_pwm(&mut timer2)` |
+
+    pub use atmega328p_hal::pwm::*;
+}
+
 pub use crate::pins::*;
 
 pub type Delay = hal::delay::Delay<hal::clock::MHz16>;
