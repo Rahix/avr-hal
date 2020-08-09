c4c1fe3e1003c8d3d1a53dccc6db11518920d6e4
Rahix <rahix@rahix.de>
2020-08-09 19:37:57 +0200

    arduino-leonardo: Properly document PWM module
    Instead of 'inheriting' the documentation from atmega32u4-hal, add more
    appropriate docs which use the `Pins` struct instead of raw PXi names.
    
    Signed-off-by: Rahix <rahix@rahix.de>

--- boards/arduino-leonardo/src/lib.rs
+++ boards/arduino-leonardo/src/lib.rs
@@ -12,7 +12,39 @@ pub use crate::atmega32u4::Peripherals;
 pub use atmega32u4_hal::prelude;
 pub use atmega32u4_hal::spi;
 pub use atmega32u4_hal::adc;
-pub use atmega32u4_hal::pwm;
+
+pub mod pwm {
+    //! Support for PWM pins
+    //!
+    //! The 4 timers of ATmega32U4 can be used for PWM on certain pins.
+    //! The PWM methods are from `embedded_hal::PwmPin`.
+    //!
+    //! # Example
+    //! ```
+    //! let mut pins = arduino_leonardo::Pins::new(dp.PORTB, dp.PORTC, dp.PORTD, dp.PORTE);
+    //! let mut timer1 = Timer1Pwm::new(dp.TC1);
+    //!
+    //! let mut d3 = pins.d3.into_output(&mut pins.ddr).into_pwm(&mut timer0);
+    //!
+    //! d3.set_duty(128);
+    //! d3.enable();
+    //! ```
+    //!
+    //! Here is an overview of pins and which timer they work with:
+    //!
+    //! | Pin | Conversion Method | Alternate Conversion Method |
+    //! | --- | --- | --- |
+    //! | `pins.d3` | `.into_pwm(&mut timer0)` | |
+    //! | `pins.d5` | `.into_pwm(&mut timer3)` | |
+    //! | `pins.d6` | `.into_pwm(&mut timer4)` | |
+    //! | `pins.d9` | `.into_pwm(&mut timer1)` | |
+    //! | `pins.d10` | `.into_pwm(&mut timer1)` | `.into_pwm4(&mut timer4)` |
+    //! | `pins.d11` | `.into_pwm(&mut timer0)` | `.into_pwm1(&mut timer1)` |
+    //! | `pins.d13` | `.into_pwm(&mut timer4)` | |
+
+    pub use atmega32u4_hal::pwm::*;
+}
+
 pub use crate::pins::*;
 
 pub type Delay = hal::delay::Delay<hal::clock::MHz16>;
