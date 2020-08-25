1913d1878deeadd536a6fe716b190cbbc8a7b118
Kyle Eli <keli@advancedsolutions.com>
2020-08-25 10:30:21 -0400

    atmega328p wdt: define timeout periods in chip

--- avr-hal-generic/src/wdt.rs
+++ avr-hal-generic/src/wdt.rs
@@ -1,49 +1,30 @@
 //! WDT Implementation
 
-pub use embedded_hal::watchdog;
 
-/// Approximate length of the time-out period before the watchdog provides a system reset.
-///
-/// After enabling the watchdog timer, call [`Watchdog::feed`] before the period ends to prevent a
-/// reset.
-///
-/// Some AVR models do not support all period lengths.
-///
-/// [`Watchdog::feed`]: watchdog/trait.Watchdog.html#tymethod.feed
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub enum WatchdogTimeOutPeriod {
-    /// 16 milliseconds
-    Ms16,
-    /// 32 milliseconds
-    Ms32,
-    /// 64 milliseconds
-    Ms64,
-    /// 125 milliseconds
-    Ms125,
-    /// 250 milliseconds
-    Ms250,
-    /// 500 milliseconds
-    Ms500,
-    /// 1 second
-    Ms1000,
-    /// 2 seconds
-    Ms2000,
-    /// 4 seconds
-    Ms4000,
-    /// 8 seconds
-    Ms8000,
-}
+pub use embedded_hal::watchdog;
 
 /// Implement traits for the watchdog interface
 #[macro_export]
 macro_rules! impl_wdt {
     (
+        pub enum WatchdogTimeOutPeriod $supported_periods:tt
+
         $(#[$wdt_attr:meta])*
         pub struct $Wdt:ident {
             mcu_status_register: $MCUSR:ty,
             peripheral: $WDT:ty,
+            prescaler_bits: {$($period:path => $($bits:ident()).+ ),+}
         }
     ) => {
+        /// Approximate length of the time-out period before the watchdog provides a system reset.
+        ///
+        /// After enabling the watchdog timer, call [`Watchdog::feed`] before the period ends to prevent a
+        /// reset.
+        ///
+        /// [`Watchdog::feed`]: watchdog/trait.Watchdog.html#tymethod.feed
+        #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+        pub enum WatchdogTimeOutPeriod $supported_periods
+
         /// Provides a system reset when a counter reaches a given time-out value.
         ///
         /// # Note
@@ -102,20 +83,7 @@ macro_rules! impl_wdt {
                     // Enable watchdog and set interval
                     self.peripheral.wdtcsr.write(|w| {
                         (match period.into() {
-                            WatchdogTimeOutPeriod::Ms16 => w.wdpl().cycles_2k_512k(),
-                            WatchdogTimeOutPeriod::Ms32 => w.wdpl().cycles_4k_1024k(),
-                            WatchdogTimeOutPeriod::Ms64 => w.wdpl().cycles_8k(),
-                            WatchdogTimeOutPeriod::Ms125 => w.wdpl().cycles_16k(),
-                            WatchdogTimeOutPeriod::Ms250 => w.wdpl().cycles_32k(),
-                            WatchdogTimeOutPeriod::Ms500 => w.wdpl().cycles_64k(),
-                            WatchdogTimeOutPeriod::Ms1000 => w.wdpl().cycles_128k(),
-                            WatchdogTimeOutPeriod::Ms2000 => w.wdpl().cycles_256k(),
-                            WatchdogTimeOutPeriod::Ms4000 => {
-                                w.wdpl().cycles_2k_512k().wdph().set_bit()
-                            }
-                            WatchdogTimeOutPeriod::Ms8000 => {
-                                w.wdpl().cycles_4k_1024k().wdph().set_bit()
-                            }
+                            $($period => w.$($bits()).+),+
                         })
                         .wde()
                         .set_bit()
--- chips/atmega328p-hal/src/wdt.rs
+++ chips/atmega328p-hal/src/wdt.rs
@@ -4,8 +4,44 @@ pub use avr_hal_generic::wdt::watchdog::*;
 pub use avr_hal_generic::wdt::*;
 
 avr_hal::impl_wdt! {
+
+    pub enum WatchdogTimeOutPeriod {
+        /// 16 milliseconds
+        Ms16,
+        /// 32 milliseconds
+        Ms32,
+        /// 64 milliseconds
+        Ms64,
+        /// 125 milliseconds
+        Ms125,
+        /// 250 milliseconds
+        Ms250,
+        /// 500 milliseconds
+        Ms500,
+        /// 1 second
+        Ms1000,
+        /// 2 seconds
+        Ms2000,
+        /// 4 seconds
+        Ms4000,
+        /// 8 seconds
+        Ms8000,
+    }
+
     pub struct Wdt {
         mcu_status_register: crate::atmega328p::cpu::MCUSR,
         peripheral: crate::atmega328p::WDT,
+        prescaler_bits: {
+            WatchdogTimeOutPeriod::Ms16 => wdpl().cycles_2k_512k(),
+            WatchdogTimeOutPeriod::Ms32 => wdpl().cycles_4k_1024k(),
+            WatchdogTimeOutPeriod::Ms64 => wdpl().cycles_8k(),
+            WatchdogTimeOutPeriod::Ms125 => wdpl().cycles_16k(),
+            WatchdogTimeOutPeriod::Ms250 => wdpl().cycles_32k(),
+            WatchdogTimeOutPeriod::Ms500 => wdpl().cycles_64k(),
+            WatchdogTimeOutPeriod::Ms1000 => wdpl().cycles_128k(),
+            WatchdogTimeOutPeriod::Ms2000 => wdpl().cycles_256k(),
+            WatchdogTimeOutPeriod::Ms4000 => wdpl().cycles_2k_512k().wdph().set_bit(),
+            WatchdogTimeOutPeriod::Ms8000 => wdpl().cycles_4k_1024k().wdph().set_bit()
+        }
     }
 }
