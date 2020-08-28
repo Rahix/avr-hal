1ac209eda9e35bf0c1cb7da198466b037b11bc82
Rahix <rahix@rahix.de>
2020-08-28 14:30:25 +0200

    arduino-leonardo: Add watchdog module and example
    Signed-off-by: Rahix <rahix@rahix.de>

---
+++ boards/arduino-leonardo/examples/leonardo-watchdog.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+use arduino_leonardo::prelude::*;
+use arduino_leonardo::wdt;
+
+#[arduino_leonardo::entry]
+fn main() -> ! {
+    let dp = arduino_leonardo::Peripherals::take().unwrap();
+
+    let mut pins = arduino_leonardo::Pins::new(
+        dp.PORTB,
+        dp.PORTC,
+        dp.PORTD,
+        dp.PORTE,
+    );
+
+    let mut led = pins.d13.into_output(&mut pins.ddr);
+
+    let mut watchdog = wdt::Wdt::new(&dp.CPU.mcusr, dp.WDT);
+    watchdog.start(wdt::Timeout::Ms2000);
+
+    loop {
+        led.toggle().void_unwrap();
+        arduino_leonardo::delay_ms(500);
+        watchdog.feed();
+    }
+}
--- boards/arduino-leonardo/src/lib.rs
+++ boards/arduino-leonardo/src/lib.rs
@@ -213,3 +213,24 @@ pub type Serial<IMODE> = hal::usart::Usart1<hal::clock::MHz16, IMODE>;
 ///
 /// [ex-i2c]: https://github.com/Rahix/avr-hal/blob/master/boards/arduino-leonardo/examples/leonardo-i2cdetect.rs
 pub type I2c<M> = hal::i2c::I2c<hal::clock::MHz16, M>;
+
+/// Support for the Watchdog Timer
+///
+/// # Note
+/// Changing the watchdog configuration requires two separate writes to WDTCSR where the second
+/// write must occur within 4 cycles of the first or the configuration will not change. You may need
+/// to adjust optimization settings to prevent other operations from being emitted between these two
+/// writes.
+///
+/// # Example
+/// ```
+/// let mut watchdog = arduino_leonardo::wdt::Wdt::new(&dp.CPU.mcusr, dp.WDT);
+/// watchdog.start(arduino_leonardo::wdt::Timeout::Ms8000);
+///
+/// loop {
+///     watchdog.feed();
+/// }
+/// ```
+pub mod wdt {
+    pub use atmega32u4_hal::wdt::*;
+}
