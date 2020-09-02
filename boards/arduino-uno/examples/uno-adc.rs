21d7842ad8c5bcb259210262e737d887ab120c38
Rahix <rahix@rahix.de>
2020-09-02 09:22:02 +0200

    arduino-uno: Improve ADC example
    Show off all pins and channels in the Arduino Uno ADC example.
    
    Signed-off-by: Rahix <rahix@rahix.de>

--- boards/arduino-uno/examples/uno-adc.rs
+++ boards/arduino-uno/examples/uno-adc.rs
@@ -5,8 +5,9 @@ use arduino_uno::adc;
 use arduino_uno::prelude::*;
 use panic_halt as _;
 
-// This example opens a serial connection to the host computer.  On most POSIX operating systems (like GNU/Linux or
-// OSX), you can interface with the program by running (assuming the device appears as ttyACM0)
+// This example opens a serial connection to the host computer.  On most POSIX operating systems
+// (like GNU/Linux or OSX), you can interface with the program by running (assuming the device
+// appears as ttyACM0)
 //
 // $ sudo screen /dev/ttyACM0 9600
 
@@ -20,12 +21,37 @@ fn main() -> ! {
         arduino_uno::Serial::new(dp.USART0, pins.d0, pins.d1.into_output(&mut pins.ddr), 9600);
 
     let mut adc = adc::Adc::new(dp.ADC, Default::default());
-    let mut a0 = pins.a0.into_analog_input(&mut adc);
 
-    ufmt::uwriteln!(&mut serial, "Reading analog input on pin A0\r").void_unwrap();
+    let (vbg, gnd): (u16, u16) = (
+        nb::block!(adc.read(&mut adc::channel::Vbg)).void_unwrap(),
+        nb::block!(adc.read(&mut adc::channel::Gnd)).void_unwrap(),
+    );
+
+    ufmt::uwriteln!(&mut serial, "Vbandgap: {}\r", vbg).void_unwrap();
+    ufmt::uwriteln!(&mut serial, "GND: {}\r", gnd).void_unwrap();
+
+    let mut a0 = pins.a0.into_analog_input(&mut adc);
+    let mut a1 = pins.a1.into_analog_input(&mut adc);
+    let mut a2 = pins.a2.into_analog_input(&mut adc);
+    let mut a3 = pins.a3.into_analog_input(&mut adc);
+    let mut a4 = pins.a4.into_analog_input(&mut adc);
+    let mut a5 = pins.a5.into_analog_input(&mut adc);
 
     loop {
-        let value: u16 = nb::block!(adc.read(&mut a0)).void_unwrap();
-        ufmt::uwriteln!(&mut serial, "read: {}\r", value).void_unwrap();
+        let values: [u16; 6] = [
+            nb::block!(adc.read(&mut a0)).void_unwrap(),
+            nb::block!(adc.read(&mut a1)).void_unwrap(),
+            nb::block!(adc.read(&mut a2)).void_unwrap(),
+            nb::block!(adc.read(&mut a3)).void_unwrap(),
+            nb::block!(adc.read(&mut a4)).void_unwrap(),
+            nb::block!(adc.read(&mut a5)).void_unwrap(),
+        ];
+
+        for (i, v) in values.iter().enumerate() {
+            ufmt::uwrite!(&mut serial, "A{}: {} ", i, v).void_unwrap();
+        }
+        ufmt::uwriteln!(&mut serial, "\r").void_unwrap();
+
+        arduino_uno::delay_ms(1000);
     }
 }
