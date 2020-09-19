4d30278237f81c4f45ed115de574efc2c5f7f267
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-09-19 12:16:33 +0200

    Remove some pins and add doc

--- boards/sparkfun-pro-micro/src/pins.rs
+++ boards/sparkfun-pro-micro/src/pins.rs
@@ -15,18 +15,6 @@ avr_hal_generic::impl_board_pins! {
 
     /// Reexport of the Pro Micro's pins, with the names they have on the board
     pub struct Pins {
-        /// `A0`
-        pub a0: portf::pf7::PF7,
-        /// `A1`
-        pub a1: portf::pf6::PF6,
-        /// `A2`
-        pub a2: portf::pf5::PF5,
-        /// `A3`
-        pub a3: portf::pf4::PF4,
-        /// `A4`
-        pub a4: portf::pf1::PF1,
-        /// `A5`
-        pub a5: portf::pf0::PF0,
         /// `D0` / `RX`
         ///
         /// * `RX` (UART)
@@ -116,5 +104,21 @@ avr_hal_generic::impl_board_pins! {
         ///
         /// ICSP MISO pin
         pub miso: portb::pb3::PB3,
+        /// `A0`
+        ///
+        /// * `ADC7` channel
+        pub a0: portf::pf7::PF7,
+        /// `A1`
+        ///
+        /// * `ADC6` channel
+        pub a1: portf::pf6::PF6,
+        /// `A2`
+        ///
+        /// * `ADC5` channel
+        pub a2: portf::pf5::PF5,
+        /// `A3`
+        ///
+        /// * `ADC4` channel
+        pub a3: portf::pf4::PF4,
     }
 }
