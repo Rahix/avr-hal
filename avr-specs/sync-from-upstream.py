6c29fe570edac956b7d9082637e68a8919df8c82
Jan Paw <jan.paw@tesco.com>
2020-10-08 20:37:26 +0200

    apply remarks for sync-from-upstream.py

--- avr-specs/sync-from-upstream.py
+++ avr-specs/sync-from-upstream.py
@@ -2,62 +2,56 @@
 
 import subprocess
 import json
+import copy
 import os
 
-ENCODING = 'utf-8'
+SPECS = {
+    "atmega32u4": {
+        "cpu": "atmega32u4",
+    },
+    "atmega48p": {
+        "cpu": "atmega48p",
+    },
+    "atmega168": {
+        "cpu": "atmega168",
+    },
+    "atmega328p": {
+        "cpu": "atmega328p",
+    },
+    "atmega1280": {
+        "cpu": "atmega1280",
+    },
+    "atmega2560": {
+        "cpu": "atmega2560",
+    },
+    "attiny85": {
+        "cpu": "attiny85",
+    },
+    "attiny88": {
+        "cpu": "attiny88",
+    },
+}
 
 
 def main():
-    ref_spec_string = subprocess.run(
-        ['rustc', '--print', 'target-spec-json', '-Z', 'unstable-options', '--target', 'avr-unknown-gnu-atmega328'],
+    upstream_spec_string = subprocess.run(
+        ['rustc', '--print', 'target-spec-json', '-Z',
+            'unstable-options', '--target', 'avr-unknown-gnu-atmega328'],
         check=True,
         stdout=subprocess.PIPE,
     ).stdout
 
-    if ref_spec_string is not None:
-        ref_spec = json.loads(ref_spec_string)
+    if upstream_spec_string is not None:
+        upstream_spec = json.loads(upstream_spec_string)
 
-        specs_path = 'avr-specs'
-        paths = find_spec_paths(specs_path)
+    for mcu, settings in SPECS.items():
+        spec = copy.deepcopy(upstream_spec)
+        spec.update(settings)
+        spec['pre-link-args']['gcc'][0] = '-mmcu=%s' % mcu
 
-        for spec_path in paths:
-            legacy_spec = read_spec(spec_path)
-            new_spec = merge_specs(legacy_spec, ref_spec)
-            write_spec(spec_path, new_spec)
-    else:
-        print(stderr)
-
-
-def find_spec_paths(base_path):
-    paths = []
-    for i in os.listdir(base_path):
-        if i.endswith('.json'):
-            full_path = '%s/%s' % (base_path, i)
-            paths.append(full_path)
-    return paths
-
-
-def read_spec(path):
-    legacy_file = open(path, 'r', encoding=ENCODING)
-    legacy_string = legacy_file.read()
-    legacy_file.close()
-    return json.loads(legacy_string)
-
-
-def write_spec(spec_path, spec):
-    legacy_file = open(spec_path, 'w',  encoding=ENCODING)
-    json.dump(spec, legacy_file, sort_keys=True, indent=2)
-    legacy_file.write('\n')
-    legacy_file.close()
-
-
-def merge_specs(legacy, ref):
-    cpu = legacy['cpu']
-    data_layout = legacy['data-layout']
-    ref['cpu'] = cpu
-    ref['data-layout'] = data_layout
-    ref['pre-link-args']['gcc'][0] = '-mmcu=%s' % cpu
-    return ref
+        with open(f"avr-specs/avr-{mcu}.json", "w") as f:
+            json.dump(spec, f, sort_keys=True, indent=2)
+            f.write("\n")
 
 
 if __name__ == "__main__":
