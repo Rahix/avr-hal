665ddf72fdbdc5a099c17e6291b3f12b741567b4
Jan Paw <jan.paw@tesco.com>
2020-10-10 21:29:39 +0200

    leonardo runner - support for macos

--- boards/arduino-leonardo/leonardo-runner.sh
+++ boards/arduino-leonardo/leonardo-runner.sh
@@ -1,6 +1,39 @@
 #!/usr/bin/env sh
 set -e
 
+LINUX_OS="LINUX"
+MAC_OS="MAC"
+UNKNOWN_OS="UNKNOWN"
+
+case "$(uname -s)" in
+    Linux*)     OS=$LINUX_OS;;
+    Darwin*)    OS=$MAC_OS;;
+    *)          OS=$UNKNOWN_OS
+esac
+
+if ! command -v numfmt &> /dev/null
+then
+    echo "required numfmt could not be found!"
+    echo "please install https://command-not-found.com/numfmt"
+    exit 1
+fi
+
+if ! command -v avrdude &> /dev/null
+then
+    echo "required avrdude could not be found!"
+    echo "please install https://command-not-found.com/avrdude"
+    exit 1
+fi
+
+if [ $OS = $LINUX_OS ]; then
+    ARDUINO_UPLOAD_PORT="/dev/ttyACM0"
+elif [ $OS = $MAC_OS ]; then
+    ARDUINO_UPLOAD_PORT="/dev/cu.usbmodem146201"
+else
+    echo "Not supporte OS!"
+    exit 1    
+fi
+
 if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
     echo "usage: $0 <application.elf>" >&2
     exit 1
@@ -28,4 +61,4 @@ read -r
 printf "Attempting to flash ...\n"
 printf "\n"
 
-avrdude -qq -C/etc/avrdude.conf -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D "-Uflash:w:$1:e"
+avrdude -qq -patmega32u4 -cavr109 -P"${ARDUINO_UPLOAD_PORT}" -b57600 -D "-Uflash:w:$1:e"
