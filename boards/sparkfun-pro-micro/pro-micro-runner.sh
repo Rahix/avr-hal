a8570b489c951b3cb5abfbf415372538cacdad2c
Jan Paw <jan.paw@tesco.com>
2020-10-12 21:57:27 +0200

    uno an pro-micro runner update

--- boards/sparkfun-pro-micro/pro-micro-runner.sh
+++ boards/sparkfun-pro-micro/pro-micro-runner.sh
@@ -1,6 +1,35 @@
 #!/usr/bin/env sh
 set -e
 
+case "$(uname -s)" in
+    Linux*)     OS="Linux";;
+    Darwin*)    OS="Mac";;
+    *)          OS="Unknown";;
+esac
+
+if ! command -v numfmt &> /dev/null
+then
+    echo "numfmt is needed for human-readable sizes." >&2
+    echo "please install https://command-not-found.com/numfmt" >&2
+    alias numfmt=true
+fi
+
+if ! command -v avrdude &> /dev/null
+then
+    echo "required avrdude could not be found!" >&2
+    echo "please install https://command-not-found.com/avrdude" >&2
+    exit 1
+fi
+
+if [ $OS = "Linux" ]; then
+    SERIAL_PORT="/dev/ttyACM0"
+elif [ $OS = "Mac" ]; then
+    SERIAL_PORT="/dev/cu.usbmodem146201"
+else
+    echo "unsupported OS, things might not work" >&2
+    SERIAL_PORT="/dev/ttyACM0"
+fi
+
 if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
     echo "usage: $0 <application.elf>" >&2
     exit 1
@@ -28,4 +57,4 @@ read -r
 printf "Attempting to flash ...\n"
 printf "\n"
 
-avrdude -qq -C/etc/avrdude.conf -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D "-Uflash:w:$1:e"
+avrdude -qq  -patmega32u4 -cavr109 -P"${SERIAL_PORT}" -b57600 -D "-Uflash:w:$1:e"
