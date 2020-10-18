3df75585e862dfa40c37c915e21cefccfb90c0d9
Rahix <rahix@rahix.de>
2020-10-18 16:46:41 +0200

    atmega1280: Import auto-generated module as `pac`
    In an attempt to make all HAL crates look more similar, adopt the
    stm32-rs convention of importing the svd2rust generated module as `pac`.
    
    Signed-off-by: Rahix <rahix@rahix.de>

--- chips/atmega1280-hal/src/lib.rs
+++ chips/atmega1280-hal/src/lib.rs
@@ -1,6 +1,8 @@
 #![no_std]
 
-pub use avr_device::atmega1280;
+/// Reexport of `atemga1280` from `avr-device`
+pub use avr_device::atmega1280 as pac;
+
 /// See [`avr_device::entry`](https://docs.rs/avr-device/latest/avr_device/attr.entry.html).
 #[cfg(feature = "rt")]
 pub use avr_device::entry;
@@ -22,7 +24,7 @@ pub mod i2c {
 
     avr_hal_generic::impl_twi_i2c! {
         pub struct I2c {
-            peripheral: crate::atmega1280::TWI,
+            peripheral: crate::pac::TWI,
             pins: {
                 sda: portd::PD1,
                 scl: portd::PD0,
--- chips/atmega1280-hal/src/usart.rs
+++ chips/atmega1280-hal/src/usart.rs
@@ -5,7 +5,7 @@ use crate::port::portj;
 
 avr_hal_generic::impl_usart! {
     pub struct Usart0 {
-        peripheral: crate::atmega1280::USART0,
+        peripheral: crate::pac::USART0,
         pins: {
             rx: porte::PE0,
             tx: porte::PE1,
@@ -16,7 +16,7 @@ avr_hal_generic::impl_usart! {
 
 avr_hal_generic::impl_usart! {
     pub struct Usart1 {
-        peripheral: crate::atmega1280::USART1,
+        peripheral: crate::pac::USART1,
         pins: {
             rx: portd::PD2,
             tx: portd::PD3,
@@ -27,7 +27,7 @@ avr_hal_generic::impl_usart! {
 
 avr_hal_generic::impl_usart! {
     pub struct Usart2 {
-        peripheral: crate::atmega1280::USART2,
+        peripheral: crate::pac::USART2,
         pins: {
             rx: porth::PH0,
             tx: porth::PH1,
@@ -38,7 +38,7 @@ avr_hal_generic::impl_usart! {
 
 avr_hal_generic::impl_usart! {
     pub struct Usart3 {
-        peripheral: crate::atmega1280::USART3,
+        peripheral: crate::pac::USART3,
         pins: {
             rx: portj::PJ0,
             tx: portj::PJ1,
