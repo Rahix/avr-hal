ee41658ce61ce5d88157bdfd28c2f1aeb9d4ba4b
Rahix <rahix@rahix.de>
2020-12-11 23:11:29 +0100

    generic: usart: Make macro usage internal only
    It is showing more and more that generating the entire API for the HAL
    crates from macros is a bad idea.  Reasons include:
    
     - It obscures the code.  People have trouble finding where a certain
       type is defined, especially if they are not too well aquainted with
       rust macro_rules!().
    
     - It breaks auto-completion in IDEs because they'd need to introspect
       the macros.
    
     - It leaves many types with the same methods instead of a single one.
       This also makes code that is generic over the exact peripheral
       impossible to write.
    
     - It makes it much harder to cleanly support different peripherals
       under a common interface.
    
    To mitigate these problems, shift to a different design:  A single
    `Usart` type provides the public API and is generic over the USART
    peripherals which all implement a common interface called `UsartOps`.
    Only this trait implementation will be macro generated and thus there is
    no user-facing macro-generated API left.
    
    This design also makes it much easier to implement support for multiple
    different hardware peripherals: As long as the `UsartOps` are expressive
    enough to fit all of them, we can simply provide multiple macros for
    generating the implementation for each one.

--- avr-hal-generic/src/usart.rs
+++ avr-hal-generic/src/usart.rs
@@ -1,6 +1,8 @@
 //! Serial Implementations
 
 use core::cmp::Ordering;
+use core::marker;
+use void::ResultVoidExt;
 
 // Clock is needed because the calculations needs to take core clock into account
 #[derive(Debug, Clone, Copy)]
@@ -100,291 +102,162 @@ impl BaudrateArduinoExt for u32 {
     }
 }
 
-/// Implement serial traits for a USART peripheral
+pub trait UsartOps<RX, TX> {
+    /// Enable & initialize this USART peripheral to the given baudrate.
+    fn init<CLOCK>(&mut self, baudrate: Baudrate<CLOCK>);
+    /// Disable this USART peripheral such that the pins can be used for other purposes again.
+    fn deinit(&mut self);
+
+    /// Flush all remaining data in the TX buffer.
+    ///
+    /// This operation must be non-blocking and return [`nb::Error::WouldBlock`] if not all data
+    /// was flushed yet.
+    fn flush(&mut self) -> nb::Result<(), void::Void>;
+    /// Write a byte to the TX buffer.
+    ///
+    /// This operation must be non-blocking and return [`nb::Error::WouldBlock`] until the byte is
+    /// enqueued.  The operation should not wait for the byte to have actually been sent.
+    fn write(&mut self, byte: u8) -> nb::Result<(), void::Void>;
+    /// Read a byte from the RX buffer.
+    ///
+    /// This operation must be non-blocking and return [`nb::Error::WouldBlock`] if no incoming
+    /// byte is available.
+    fn read(&mut self) -> nb::Result<u8, void::Void>;
+}
+
+pub struct Usart<USART: UsartOps<RX, TX>, RX, TX, CLOCK> {
+    p: USART,
+    rx: RX,
+    tx: TX,
+    _clock: marker::PhantomData<CLOCK>,
+}
+
+impl<USART: UsartOps<RX, TX>, RX, TX, CLOCK> Usart<USART, RX, TX, CLOCK> {
+    pub fn new(p: USART, rx: RX, tx: TX, baudrate: Baudrate<CLOCK>) -> Self {
+        let mut usart = Self {
+            p,
+            rx,
+            tx,
+            _clock: marker::PhantomData,
+        };
+        usart.p.init(baudrate);
+        usart
+    }
+
+    pub fn release(mut self) -> (USART, RX, TX) {
+        self.p.deinit();
+        (self.p, self.rx, self.tx)
+    }
+
+    pub fn flush(&mut self) {
+        nb::block!(self.p.flush()).void_unwrap()
+    }
+
+    pub fn write_byte(&mut self, byte: u8) {
+        nb::block!(self.p.write(byte)).void_unwrap()
+    }
+
+    pub fn read_byte(&mut self) -> u8 {
+        nb::block!(self.p.read()).void_unwrap()
+    }
+}
+
+impl<USART: UsartOps<RX, TX>, RX, TX, CLOCK> ufmt::uWrite for Usart<USART, RX, TX, CLOCK> {
+    type Error = void::Void;
+
+    fn write_str(&mut self, s: &str) -> Result<(), Self::Error> {
+        for b in s.as_bytes().iter() {
+            self.write_byte(*b);
+        }
+        Ok(())
+    }
+}
+
+impl<USART: UsartOps<RX, TX>, RX, TX, CLOCK> hal::serial::Write<u8>
+    for Usart<USART, RX, TX, CLOCK>
+{
+    type Error = void::Void;
+
+    fn write(&mut self, byte: u8) -> nb::Result<(), Self::Error> {
+        self.p.write(byte)
+    }
+
+    fn flush(&mut self) -> nb::Result<(), Self::Error> {
+        self.p.flush()
+    }
+}
+
+impl<USART: UsartOps<RX, TX>, RX, TX, CLOCK> hal::serial::Read<u8> for Usart<USART, RX, TX, CLOCK> {
+    type Error = void::Void;
+
+    fn read(&mut self) -> nb::Result<u8, Self::Error> {
+        self.p.read()
+    }
+}
+
 #[macro_export]
-macro_rules! impl_usart {
+macro_rules! impl_usart_traditional {
     (
-        $(#[$usart_attr:meta])*
-        pub struct $Usart:ident {
-            peripheral: $USART:ty,
-            pins: {
-                rx: $rxmod:ident::$RX:ident,
-                tx: $txmod:ident::$TX:ident,
-            },
-            register_suffix: $n:expr,
-        }
-    ) => {$crate::paste::paste! {
-        $(#[$usart_attr])*
-        pub struct $Usart<CLOCK, RX_MODE>
-        where
-            CLOCK: $crate::clock::Clock,
-            RX_MODE: $crate::port::mode::InputMode,
-        {
-            p: $USART,
-            rx: $rxmod::$RX<$crate::port::mode::Input<RX_MODE>>,
-            tx: $txmod::$TX<$crate::port::mode::Output>,
-            _clock: ::core::marker::PhantomData<CLOCK>,
-        }
+        peripheral: $USART:ty,
+        register_suffix: $n:expr,
+        rx: $rxmod:ident::$RX:ident,
+        tx: $txmod:ident::$TX:ident,
+    ) => {
+        $crate::paste::paste! {
+            impl $crate::usart::UsartOps<
+                $rxmod::$RX<$crate::port::mode::Input<$crate::port::mode::Floating>>,
+                $txmod::$TX<$crate::port::mode::Output>,
+            > for $USART {
+                fn init<CLOCK>(&mut self, baudrate: $crate::usart::Baudrate<CLOCK>) {
+                    self.[<ubrr $n>].write(|w| unsafe { w.bits(baudrate.ubrr) });
+                    self.[<ucsr $n a>].write(|w| w.[<u2x $n>]().bit(baudrate.u2x));
 
-        impl<CLOCK, RX_MODE> $Usart<CLOCK, RX_MODE>
-        where
-            CLOCK: $crate::clock::Clock,
-            RX_MODE: $crate::port::mode::InputMode,
-        {
-            /// Initialize the USART peripheral
-            ///
-            /// Please note that not all baudrates will produce a good signal
-            /// and setting it too high might make data sent completely unreadable
-            /// for the other side.
-            pub fn new(
-                p: $USART,
-                rx: $rxmod::$RX<$crate::port::mode::Input<RX_MODE>>,
-                tx: $txmod::$TX<$crate::port::mode::Output>,
-                baud: $crate::usart::Baudrate<CLOCK>,
-            ) -> $Usart<CLOCK, RX_MODE> {
-                let mut usart = $Usart {
-                    p,
-                    rx,
-                    tx,
-                    _clock: ::core::marker::PhantomData,
-                };
-                usart.initialize(baud);
-                usart
-            }
+                    // Enable receiver and transmitter but leave interrupts disabled.
+                    self.[<ucsr $n b>].write(|w| w
+                        .[<txen $n>]().set_bit()
+                        .[<rxen $n>]().set_bit()
+                    );
 
-            fn initialize(&mut self, baud: $crate::usart::Baudrate<CLOCK>) {
-                self.p.[<ubrr $n>].write(|w| unsafe { w.bits(baud.ubrr) });
-                self.p.[<ucsr $n a>].write(|w| w.[<u2x $n>]().bit(baud.u2x));
+                    // Set frame format to 8n1 for now.  At some point, this should be made
+                    // configurable, similar to what is done in other HALs.
+                    self.[<ucsr $n c>].write(|w| w
+                        .[<umsel $n>]().usart_async()
+                        .[<ucsz $n>]().chr8()
+                        .[<usbs $n>]().stop1()
+                        .[<upm $n>]().disabled()
+                    );
+                }
 
-                // Enable receiver and transmitter but leave interrupts disabled.
-                self.p.[<ucsr $n b>].write(|w| w
-                    .[<txen $n>]().set_bit()
-                    .[<rxen $n>]().set_bit()
-                );
+                fn deinit(&mut self) {
+                    // Wait for any ongoing transfer to finish.
+                    $crate::nb::block!(self.flush()).ok();
+                    self.[<ucsr $n b>].reset();
+                }
 
-                // Set frame format to 8n1 for now.  At some point, this should be made
-                // configurable, similar to what is done in other HALs.
-                self.p.[<ucsr $n c>].write(|w| w
-                    .[<umsel $n>]().usart_async()
-                    .[<ucsz $n>]().chr8()
-                    .[<usbs $n>]().stop1()
-                    .[<upm $n>]().disabled()
-                );
-            }
-
-            /// Enable/disable "RX Complete" interrupt
-            ///
-            /// When this interrupt triggers, new data is available to be read from the
-            /// data-register.  The corresponding ISR is `USARTi_RX` (where `i` is this
-            /// peripheral's number).  For example, for `USART1` on `ATmega32U4`:
-            ///
-            /// ```
-            /// #[avr_device::interrupt(atmega32u4)]
-            /// fn USART1_RX() {
-            ///     // ...
-            /// }
-            /// ```
-            pub fn interrupt_rxc(&mut self, state: bool) {
-                self.p.[<ucsr $n b>].modify(|_, w| w.[<rxcie $n>]().bit(state));
-            }
-
-            /// Enable/disable "USART Data-Register Empty" interrupt
-            ///
-            /// This interrupt signals that new data can be written to the data-register.  The
-            /// corresponding ISR is `USARTi_UDRE` (where `i` is this peripheral's number).  For
-            /// example, for `USART1` on `ATmega32U4`:
-            ///
-            /// ```
-            /// #[avr_device::interrupt(atmega32u4)]
-            /// fn USART1_UDRE() {
-            ///     // ...
-            /// }
-            /// ```
-            pub fn interrupt_udre(&mut self, state: bool) {
-                self.p.[<ucsr $n b>].modify(|_, w| w.[<txcie $n>]().bit(state));
-            }
-
-            /// Helper method for splitting this read/write object into two halves.
-            ///
-            /// The two halves returned implement the `Read` and `Write` traits, respectively.
-            pub fn split(self) -> ([<Read $Usart>]<CLOCK, RX_MODE>, [<Write $Usart>]<CLOCK>) {
-                (
-                    [<Read $Usart>] {
-                        p: unsafe { ::core::ptr::read(&self.p) },
-                        rx: self.rx,
-                        _clock: self._clock,
-                    },
-                    [<Write $Usart>] {
-                        p: self.p,
-                        tx: self.tx,
-                        _clock: self._clock,
+                fn flush(&mut self) -> $crate::nb::Result<(), $crate::void::Void> {
+                    if self.[<ucsr $n a>].read().[<udre $n>]().bit_is_clear() {
+                        Err($crate::nb::Error::WouldBlock)
+                    } else {
+                        Ok(())
                     }
-                )
-            }
-        }
+                }
 
-        impl<CLOCK, RX_MODE> $crate::hal::serial::Write<u8> for $Usart<CLOCK, RX_MODE>
-        where
-            CLOCK: $crate::clock::Clock,
-            RX_MODE: $crate::port::mode::InputMode,
-        {
-            type Error = $crate::void::Void;
+                fn write(&mut self, byte: u8) -> $crate::nb::Result<(), $crate::void::Void> {
+                    // Call flush to make sure the data-register is empty
+                    self.flush()?;
 
-            fn write(&mut self, byte: u8) -> $crate::nb::Result<(), Self::Error> {
-                // Call flush to make sure the data-register is empty
-                self.flush()?;
-
-                self.p.[<udr $n>].write(|w| unsafe { w.bits(byte) });
-                Ok(())
-            }
-
-            fn flush(&mut self) -> $crate::nb::Result<(), Self::Error> {
-                if self.p.[<ucsr $n a>].read().[<udre $n>]().bit_is_clear() {
-                    Err($crate::nb::Error::WouldBlock)
-                } else {
+                    self.[<udr $n>].write(|w| unsafe { w.bits(byte) });
                     Ok(())
                 }
-            }
-        }
 
-        impl<CLOCK, RX_MODE> $crate::ufmt::uWrite for $Usart<CLOCK, RX_MODE>
-        where
-            CLOCK: $crate::clock::Clock,
-            RX_MODE: $crate::port::mode::InputMode,
-        {
-            type Error = $crate::void::Void;
+                fn read(&mut self) -> $crate::nb::Result<u8, $crate::void::Void> {
+                    if self.[<ucsr $n a>].read().[<rxc $n>]().bit_is_clear() {
+                        return Err($crate::nb::Error::WouldBlock);
+                    }
 
-            fn write_str(&mut self, s: &str) -> ::core::result::Result<(), Self::Error> {
-                use $crate::prelude::*;
-
-                for b in s.as_bytes().iter() {
-                    $crate::nb::block!(self.write(*b))?;
-                }
-                Ok(())
-            }
-        }
-
-        impl<CLOCK, RX_MODE> $crate::hal::serial::Read<u8> for $Usart<CLOCK, RX_MODE>
-        where
-            CLOCK: $crate::clock::Clock,
-            RX_MODE: $crate::port::mode::InputMode,
-        {
-            type Error = $crate::void::Void;
-
-            fn read(&mut self) -> $crate::nb::Result<u8, Self::Error> {
-                if self.p.[<ucsr $n a>].read().[<rxc $n>]().bit_is_clear() {
-                    return Err($crate::nb::Error::WouldBlock);
-                }
-
-                Ok(self.p.[<udr $n>].read().bits())
-            }
-        }
-
-        /// The readable half of the
-        $(#[$usart_attr])*
-        pub struct [<Read $Usart>]<CLOCK, RX_MODE>
-        where
-            CLOCK: $crate::clock::Clock,
-            RX_MODE: $crate::port::mode::InputMode,
-        {
-            p: $USART,
-            rx: $rxmod::$RX<$crate::port::mode::Input<RX_MODE>>,
-            _clock: ::core::marker::PhantomData<CLOCK>,
-        }
-
-        /// The writable half of the
-        $(#[$usart_attr])*
-        pub struct [<Write $Usart>]<CLOCK>
-        where
-            CLOCK: $crate::clock::Clock,
-        {
-            p: $USART,
-            tx: $txmod::$TX<$crate::port::mode::Output>,
-            _clock: ::core::marker::PhantomData<CLOCK>,
-        }
-
-        impl<CLOCK, RX_MODE> [<Read $Usart>]<CLOCK, RX_MODE>
-        where
-            CLOCK: $crate::clock::Clock,
-            RX_MODE: $crate::port::mode::InputMode,
-        {
-            /// Puts the two "halves" of a split `Read + Write` back together.
-            pub fn reunite(self, other: [<Write $Usart>]<CLOCK>) -> $Usart<CLOCK, RX_MODE> {
-                $Usart {
-                    p: self.p,
-                    rx: self.rx,
-                    tx: other.tx,
-                    _clock: self._clock,
+                    Ok(self.[<udr $n>].read().bits())
                 }
             }
         }
-
-        impl<CLOCK> [<Write $Usart>]<CLOCK>
-        where
-            CLOCK: $crate::clock::Clock,
-        {
-            /// Puts the two "halves" of a split `Read + Write` back together.
-            pub fn reunite<RX_MODE>(self, other: [<Read $Usart>]<CLOCK, RX_MODE>) -> $Usart<CLOCK, RX_MODE>
-            where
-                RX_MODE: $crate::port::mode::InputMode,
-            {
-                other.reunite(self)
-            }
-        }
-
-        impl<CLOCK> $crate::hal::serial::Write<u8> for [<Write $Usart>]<CLOCK>
-        where
-            CLOCK: $crate::clock::Clock,
-        {
-            type Error = $crate::void::Void;
-
-            fn write(&mut self, byte: u8) -> $crate::nb::Result<(), Self::Error> {
-                // Call flush to make sure the data-register is empty
-                self.flush()?;
-
-                self.p.[<udr $n>].write(|w| unsafe { w.bits(byte) });
-                Ok(())
-            }
-
-            fn flush(&mut self) -> $crate::nb::Result<(), Self::Error> {
-                if self.p.[<ucsr $n a>].read().[<udre $n>]().bit_is_clear() {
-                    Err($crate::nb::Error::WouldBlock)
-                } else {
-                    Ok(())
-                }
-            }
-        }
-
-        impl<CLOCK> $crate::ufmt::uWrite for [<Write $Usart>]<CLOCK>
-        where
-            CLOCK: $crate::clock::Clock,
-        {
-            type Error = $crate::void::Void;
-
-            fn write_str(&mut self, s: &str) -> ::core::result::Result<(), Self::Error> {
-                use $crate::prelude::*;
-
-                for b in s.as_bytes().iter() {
-                    $crate::nb::block!(self.write(*b))?;
-                }
-                Ok(())
-            }
-        }
-
-        impl<CLOCK, RX_MODE> $crate::hal::serial::Read<u8> for [<Read $Usart>]<CLOCK, RX_MODE>
-        where
-            CLOCK: $crate::clock::Clock,
-            RX_MODE: $crate::port::mode::InputMode,
-        {
-            type Error = $crate::void::Void;
-
-            fn read(&mut self) -> $crate::nb::Result<u8, Self::Error> {
-                if self.p.[<ucsr $n a>].read().[<rxc $n>]().bit_is_clear() {
-                    return Err($crate::nb::Error::WouldBlock);
-                }
-
-                Ok(self.p.[<udr $n>].read().bits())
-            }
-        }
-    }}
+    };
 }
