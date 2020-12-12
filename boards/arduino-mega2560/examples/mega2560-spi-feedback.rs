e5b5978dfbc6dedb284f08bd373c3a33558750ae
Jan Paw <jasiek.paw@gmail.com>
2020-12-12 22:34:41 +0100

    fix test

--- boards/arduino-mega2560/examples/mega2560-spi-feedback.rs
+++ boards/arduino-mega2560/examples/mega2560-spi-feedback.rs
@@ -23,7 +23,10 @@ use arduino_mega2560::spi::{Settings, Spi};
 fn main() -> ! {
     let dp = arduino_mega2560::Peripherals::take().unwrap();
     let mut delay = arduino_mega2560::Delay::new();
-    let mut pins = arduino_mega2560::Pins::new(dp.PORTD);
+    let mut pins = arduino_mega2560::Pins::new(
+        dp.PORTA, dp.PORTB, dp.PORTC, dp.PORTD, dp.PORTE, dp.PORTF, dp.PORTG, dp.PORTH, dp.PORTJ,
+        dp.PORTK, dp.PORTL,
+    );
 
     let mut serial = arduino_mega2560::Serial::new(
         dp.USART0,
