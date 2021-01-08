1f50864e3aa1abdf96647433172973dbdaadd404
jaxter184 <jaxter184@gmail.com>
2021-01-07 22:12:29 -0600

    squash

--- chips/attiny861-hal/src/lib.rs
+++ chips/attiny861-hal/src/lib.rs
@@ -16,3 +16,13 @@ pub mod prelude {
     pub use avr_hal_generic::prelude::*;
     pub use crate::port::PortExt as _;
 }
+
+pub mod i2c {
+	use crate::port::{portc, mode};
+	struct I2c {
+		peripheral: crate::pac::USI,
+		sda: portb::PA0<mode::Input<mode::Floating>>,
+		scl: portb::PA2<mode::Input<mode::Floating>>,
+		speed: u32,
+	}
+}
