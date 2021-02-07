c50ddede71673d64e01ee3e84214623fb78ccf32
Rahix <rahix@rahix.de>
2021-02-07 17:20:51 +0100

    Add examples crate for Arduino Uno
    With a unified `arduino-hal`, we cannot keep examples in the crate in
    a convenient way.  Instead, let's add dedicated examples crate for each
    board which demonstrate usage of `arduino-hal`.
    
    Start with a blinky and usart communication example.

---
+++ examples/arduino-uno/src/bin/uno-blink.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+
+    // Digital pin 13 is also connected to an onboard LED marked "L"
+    let mut led = dp.pins.d13.into_output();
+    led.set_high();
+
+    loop {
+        led.toggle();
+        arduino_hal::delay_ms(100);
+        led.toggle();
+        arduino_hal::delay_ms(100);
+        led.toggle();
+        arduino_hal::delay_ms(100);
+        led.toggle();
+        arduino_hal::delay_ms(800);
+    }
+}
