8cffcc8ed469d07b7b1e1854ff3ac426c41f7a0f
Jan Paw <explicite@users.noreply.github.com>
2021-02-15 19:52:13 +0100

    atmega-hal: Add support for ATmega2560

--- mcu/atmega-hal/src/lib.rs
+++ mcu/atmega-hal/src/lib.rs
@@ -10,9 +10,14 @@ compile_error!(
     Please select one of the following
 
     * atmega328p
+    * atmega32u4
+    * atmega2560
     "
 );
 
+/// Reexport of `atmega2560` from `avr-device`
+#[cfg(feature = "atmega2560")]
+pub use avr_device::atmega2560 as pac;
 /// Reexport of `atmega328p` from `avr-device`
 #[cfg(feature = "atmega328p")]
 pub use avr_device::atmega328p as pac;
@@ -43,10 +48,14 @@ pub struct RawPeripheral<P>(pub(crate) P);
 #[cfg(feature = "device-selected")]
 pub struct Peripherals {
     pub pins: Pins,
-    #[cfg(feature = "atmega328p")]
+    #[cfg(any(feature = "atmega328p", feature = "atmega2560"))]
     pub USART0: RawPeripheral<pac::USART0>,
-    #[cfg(feature = "atmega32u4")]
+    #[cfg(any(feature = "atmega32u4", feature = "atmega2560"))]
     pub USART1: RawPeripheral<pac::USART1>,
+    #[cfg(feature = "atmega2560")]
+    pub USART2: RawPeripheral<pac::USART2>,
+    #[cfg(feature = "atmega2560")]
+    pub USART3: RawPeripheral<pac::USART3>,
 }
 
 #[cfg(feature = "device-selected")]
@@ -57,11 +66,20 @@ impl Peripherals {
             pins: Pins::new(dp.PORTB, dp.PORTC, dp.PORTD),
             #[cfg(feature = "atmega32u4")]
             pins: Pins::new(dp.PORTB, dp.PORTC, dp.PORTD, dp.PORTE, dp.PORTF),
+            #[cfg(feature = "atmega2560")]
+            pins: Pins::new(
+                dp.PORTA, dp.PORTB, dp.PORTC, dp.PORTD, dp.PORTE, dp.PORTF, dp.PORTG, dp.PORTH,
+                dp.PORTJ, dp.PORTK, dp.PORTL,
+            ),
 
-            #[cfg(feature = "atmega328p")]
+            #[cfg(any(feature = "atmega328p", feature = "atmega2560"))]
             USART0: RawPeripheral(dp.USART0),
-            #[cfg(feature = "atmega32u4")]
+            #[cfg(any(feature = "atmega32u4", feature = "atmega2560"))]
             USART1: RawPeripheral(dp.USART1),
+            #[cfg(feature = "atmega2560")]
+            USART2: RawPeripheral(dp.USART2),
+            #[cfg(feature = "atmega2560")]
+            USART3: RawPeripheral(dp.USART3),
         }
     }
 
--- mcu/atmega-hal/src/usart.rs
+++ mcu/atmega-hal/src/usart.rs
@@ -17,17 +17,62 @@ avr_hal_generic::impl_usart_traditional! {
     tx: port::PD1,
 }
 
-#[cfg(feature = "atmega32u4")]
+#[cfg(any(feature = "atmega32u4", feature = "atmega2560"))]
 pub type Usart1<CLOCK, IMODE> = Usart<
     crate::RawPeripheral<crate::pac::USART1>,
     port::Pin<port::mode::Input<IMODE>, port::PD2>,
     port::Pin<port::mode::Output, port::PD3>,
     CLOCK,
 >;
-#[cfg(feature = "atmega32u4")]
+#[cfg(any(feature = "atmega32u4", feature = "atmega2560"))]
 avr_hal_generic::impl_usart_traditional! {
     peripheral: crate::RawPeripheral<crate::pac::USART1>,
     register_suffix: 1,
     rx: port::PD2,
     tx: port::PD3,
+}
+
+#[cfg(feature = "atmega2560")]
+pub type Usart0<CLOCK, IMODE> = Usart<
+    crate::RawPeripheral<crate::pac::USART0>,
+    port::Pin<port::mode::Input<IMODE>, port::PE0>,
+    port::Pin<port::mode::Output, port::PE1>,
+    CLOCK,
+>;
+#[cfg(feature = "atmega2560")]
+avr_hal_generic::impl_usart_traditional! {
+    peripheral: crate::RawPeripheral<crate::pac::USART0>,
+    register_suffix: 0,
+    rx: port::PE0,
+    tx: port::PE1,
+}
+
+#[cfg(feature = "atmega2560")]
+pub type Usart2<CLOCK, IMODE> = Usart<
+    crate::RawPeripheral<crate::pac::USART2>,
+    port::Pin<port::mode::Input<IMODE>, port::PH0>,
+    port::Pin<port::mode::Output, port::PH1>,
+    CLOCK,
+>;
+#[cfg(feature = "atmega2560")]
+avr_hal_generic::impl_usart_traditional! {
+    peripheral: crate::RawPeripheral<crate::pac::USART2>,
+    register_suffix: 2,
+    rx: port::PH0,
+    tx: port::PH1,
+}
+
+#[cfg(feature = "atmega2560")]
+pub type Usart3<CLOCK, IMODE> = Usart<
+    crate::RawPeripheral<crate::pac::USART3>,
+    port::Pin<port::mode::Input<IMODE>, port::PJ0>,
+    port::Pin<port::mode::Output, port::PJ1>,
+    CLOCK,
+>;
+#[cfg(feature = "atmega2560")]
+avr_hal_generic::impl_usart_traditional! {
+    peripheral: crate::RawPeripheral<crate::pac::USART3>,
+    register_suffix: 3,
+    rx: port::PJ0,
+    tx: port::PJ1,
 }
