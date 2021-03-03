e375d56a00c60e4461dc15d31cb01cba14e524c6
Rahix <rahix@rahix.de>
2021-03-03 21:35:29 +0100

    atmega-hal: Add I2C support
    Use the newly redesigned I2C HAL from avr-hal-generic and instantiate
    the driver for each TWI peripheral of all supported MCUs.

---
+++ mcu/atmega-hal/src/i2c.rs
@@ -0,0 +1,66 @@
+#[allow(unused_imports)]
+use crate::port;
+pub use avr_hal_generic::i2c::*;
+
+#[cfg(any(feature = "atmega1280", feature = "atmega2560", feature = "atmega32u4"))]
+pub type I2c<CLOCK> = avr_hal_generic::i2c::I2c<
+    crate::Atmega,
+    crate::pac::TWI,
+    port::Pin<port::mode::Input, port::PD1>,
+    port::Pin<port::mode::Input, port::PD0>,
+    CLOCK,
+>;
+#[cfg(any(feature = "atmega1280", feature = "atmega2560", feature = "atmega32u4"))]
+avr_hal_generic::impl_i2c_twi! {
+    hal: crate::Atmega,
+    peripheral: crate::pac::TWI,
+    sda: port::PD1,
+    scl: port::PD0,
+}
+
+#[cfg(any(feature = "atmega328p", feature = "atmega168", feature = "atmega48p"))]
+pub type I2c<CLOCK> = avr_hal_generic::i2c::I2c<
+    crate::Atmega,
+    crate::pac::TWI,
+    port::Pin<port::mode::Input, port::PC4>,
+    port::Pin<port::mode::Input, port::PC5>,
+    CLOCK,
+>;
+#[cfg(any(feature = "atmega328p", feature = "atmega168", feature = "atmega48p"))]
+avr_hal_generic::impl_i2c_twi! {
+    hal: crate::Atmega,
+    peripheral: crate::pac::TWI,
+    sda: port::PC4,
+    scl: port::PC5,
+}
+
+#[cfg(any(feature = "atmega328pb"))]
+pub type I2c0<CLOCK> = avr_hal_generic::i2c::I2c<
+    crate::Atmega,
+    crate::pac::TWI0,
+    port::Pin<port::mode::Input, port::PC4>,
+    port::Pin<port::mode::Input, port::PC5>,
+    CLOCK,
+>;
+#[cfg(any(feature = "atmega328pb"))]
+avr_hal_generic::impl_i2c_twi! {
+    hal: crate::Atmega,
+    peripheral: crate::pac::TWI0,
+    sda: port::PC4,
+    scl: port::PC5,
+}
+#[cfg(any(feature = "atmega328pb"))]
+pub type I2c1<CLOCK> = avr_hal_generic::i2c::I2c<
+    crate::Atmega,
+    crate::pac::TWI1,
+    port::Pin<port::mode::Input, port::PE0>,
+    port::Pin<port::mode::Input, port::PE1>,
+    CLOCK,
+>;
+#[cfg(any(feature = "atmega328pb"))]
+avr_hal_generic::impl_i2c_twi! {
+    hal: crate::Atmega,
+    peripheral: crate::pac::TWI1,
+    sda: port::PE0,
+    scl: port::PE1,
+}
--- mcu/atmega-hal/src/lib.rs
+++ mcu/atmega-hal/src/lib.rs
@@ -61,6 +61,11 @@ pub mod usart;
 #[cfg(feature = "device-selected")]
 pub use usart::Usart;
 
+#[cfg(feature = "device-selected")]
+pub mod i2c;
+#[cfg(feature = "device-selected")]
+pub use i2c::I2c;
+
 pub struct Atmega;
 
 #[cfg(any(feature = "atmega48p", feature = "atmega168", feature = "atmega328p"))]
