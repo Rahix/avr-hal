aac39a2410bbbef27fdf87d19d735f0cb51b0a95
David Morrison <drmorr@evokewonder.com>
2021-03-24 20:34:39 -0700

    Add explicit support for arduino-nano
    1. Add the arduino-nano feature flag to arduino-hal.
    2. Make the ADC6 and ADC7 pins only available for nano, not for uno.
    3. Add an example crate for arduino-nano with just a blink and ADC example.

--- arduino-hal/src/clock.rs
+++ arduino-hal/src/clock.rs
@@ -1,10 +1,11 @@
 pub use avr_hal_generic::clock::*;
 
 pub(crate) mod default {
-    #[cfg(feature = "arduino-uno")]
-    pub type DefaultClock = avr_hal_generic::clock::MHz16;
-    #[cfg(feature = "arduino-leonardo")]
-    pub type DefaultClock = avr_hal_generic::clock::MHz16;
-    #[cfg(feature = "arduino-mega2560")]
+    #[cfg(any(
+        feature = "arduino-leonardo",
+        feature = "arduino-mega2560",
+        feature = "arduino-nano",
+        feature = "arduino-uno",
+    ))]
     pub type DefaultClock = avr_hal_generic::clock::MHz16;
 }
