e555033804b0df5deaf8a58b6b5ff7f2aecfeee1
Jan Paw <explicite@users.noreply.github.com>
2021-07-28 21:06:46 +0200

    Update arduino-hal/src/clock.rs

--- arduino-hal/src/clock.rs
+++ arduino-hal/src/clock.rs
@@ -25,6 +25,6 @@ pub(crate) mod default {
         feature = "nano168",
     ))]
     pub type DefaultClock = avr_hal_generic::clock::MHz16;
-    #[cfg(feature = "adafruit-trinket")]
+    #[cfg(feature = "trinket")]
     pub type DefaultClock = avr_hal_generic::clock::MHz8;
 }
