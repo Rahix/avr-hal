cd32fe7c925b1e7763f18b64f9209e64eaad2209
dalpil <47555+dalpil@users.noreply.github.com>
2022-05-15 12:21:09 +0200

    No simple_pwm on attiny yet

--- arduino-hal/src/lib.rs
+++ arduino-hal/src/lib.rs
@@ -113,7 +113,7 @@ pub use delay::{delay_ms, delay_us, Delay};
 #[cfg(feature = "board-selected")]
 pub mod port;
 
-#[cfg(feature = "board-selected")]
+#[cfg(feature = "mcu-atmega")]
 pub mod simple_pwm;
 
 #[doc(no_inline)]
