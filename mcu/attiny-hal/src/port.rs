1aacefb335517f85d0de858231e11055d9768cdf
Quentin Smith <quentin@mit.edu>
2022-07-04 16:13:55 -0400

    attiny-hal: Add ATtiny167 and ADC support for all chips

--- mcu/attiny-hal/src/port.rs
+++ mcu/attiny-hal/src/port.rs
@@ -1,3 +1,30 @@
+#[cfg(feature = "attiny167")]
+avr_hal_generic::impl_port_traditional! {
+    enum Ports {
+        PORTA: (crate::pac::PORTA, porta, pina, ddra),
+        PORTB: (crate::pac::PORTB, portb, pinb, ddrb),
+    }
+
+    pub struct Pins {
+        pa0: PA0 = (crate::pac::PORTA, PORTA, 0, porta, pina, ddra),
+        pa1: PA1 = (crate::pac::PORTA, PORTA, 1, porta, pina, ddra),
+        pa2: PA2 = (crate::pac::PORTA, PORTA, 2, porta, pina, ddra),
+        pa3: PA3 = (crate::pac::PORTA, PORTA, 3, porta, pina, ddra),
+        pa4: PA4 = (crate::pac::PORTA, PORTA, 4, porta, pina, ddra),
+        pa5: PA5 = (crate::pac::PORTA, PORTA, 5, porta, pina, ddra),
+        pa6: PA6 = (crate::pac::PORTA, PORTA, 6, porta, pina, ddra),
+        pa7: PA7 = (crate::pac::PORTA, PORTA, 7, porta, pina, ddra),
+        pb0: PB0 = (crate::pac::PORTB, PORTB, 0, portb, pinb, ddrb),
+        pb1: PB1 = (crate::pac::PORTB, PORTB, 1, portb, pinb, ddrb),
+        pb2: PB2 = (crate::pac::PORTB, PORTB, 2, portb, pinb, ddrb),
+        pb3: PB3 = (crate::pac::PORTB, PORTB, 3, portb, pinb, ddrb),
+        pb4: PB4 = (crate::pac::PORTB, PORTB, 4, portb, pinb, ddrb),
+        pb5: PB5 = (crate::pac::PORTB, PORTB, 5, portb, pinb, ddrb),
+        pb6: PB6 = (crate::pac::PORTB, PORTB, 6, portb, pinb, ddrb),
+        pb7: PB7 = (crate::pac::PORTB, PORTB, 7, portb, pinb, ddrb),
+    }
+}
+
 #[cfg(feature = "attiny84")]
 avr_hal_generic::impl_port_traditional! {
     enum Ports {
