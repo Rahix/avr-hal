Rahix/avr-hal

commit d6daf71813add808a156ba0c433d1c867d3df2c9

Improve documentation
Signed-off-by: Rahix <rahix@rahix.de>

avr-hal-generic/src/delay.rs
@@ -1,6 +1,22 @@
+//! Delay implementations
+
 use core::marker;
 use hal::blocking::delay;
 
+/// A busy-loop delay implementation
+///
+/// # Example
+/// ```rust
+/// let mut delay = delay::Delay::<clock::MHz16>::new();
+///
+/// // Wait 1 second
+/// delay.delay_ms(1000);
+/// ```
+///
+/// # Warning
+/// The delay is not accurate for values above 4095µs because of a loop whose
+/// overhead is not accounted for.  This will be fixed in a future version.
+#[derive(Debug, Clone, Copy)]
 pub struct Delay<SPEED> {
     _speed: marker::PhantomData<SPEED>,
 }

avr-hal-generic/src/clock.rs
@@ -1,38 +1,48 @@
+//! Generic trait for clock speeds
+
+/// A clock speed
 pub trait Clock {
+    /// Frequency of this clock in Hz
     const FREQ: u32;
 }
 
 /// 24 MHz Clock
+#[derive(ufmt::derive::uDebug, Debug)]
 pub struct MHz24;
 impl Clock for MHz24 {
     const FREQ: u32 = 24_000_000;
 }
 
 /// 20 MHz Clock
+#[derive(ufmt::derive::uDebug, Debug)]
 pub struct MHz20;
 impl Clock for MHz20 {
     const FREQ: u32 = 20_000_000;
 }
 
 /// 16 MHz Clock
+#[derive(ufmt::derive::uDebug, Debug)]
 pub struct MHz16;
 impl Clock for MHz16 {
     const FREQ: u32 = 16_000_000;
 }
 
 /// 12 MHz Clock
+#[derive(ufmt::derive::uDebug, Debug)]
 pub struct MHz12;
 impl Clock for MHz12 {
     const FREQ: u32 = 12_000_000;
 }
 
 /// 8 MHz Clock
+#[derive(ufmt::derive::uDebug, Debug)]
 pub struct MHz8;
 impl Clock for MHz8 {
     const FREQ: u32 = 8_000_000;
 }
 
 /// 1 MHz Clock
+#[derive(ufmt::derive::uDebug, Debug)]
 pub struct MHz1;
 impl Clock for MHz1 {
     const FREQ: u32 = 1_000_000;

avr-hal-generic/src/i2c.rs
@@ -1,3 +1,5 @@
+//! I2C Implementations
+
 /// TWI Status Codes
 pub mod twi_status {
     // The status codes defined in the C header are meant to be used with the
@@ -95,18 +97,27 @@ pub mod twi_status {
     pub const TW_BUS_ERROR: u8 = 0x00 >> 3;
 }
 
+/// I2C Error
 #[derive(ufmt::derive::uDebug, Debug, Clone, Copy, Eq, PartialEq)]
 pub enum Error {
+    /// Lost arbitration while trying to acquire bus
     ArbitrationLost,
+    /// No slave answered for this address or a slave replied NACK
     AddressNack,
+    /// Slave replied NACK to sent data
     DataNack,
+    /// A bus-error occured
     BusError,
+    /// An unknown error occured.  The bus might be in an unknown state.
     Unknown,
 }
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+/// I2C Transfer Direction
+#[derive(ufmt::derive::uDebug, Debug, Clone, Copy, Eq, PartialEq)]
 pub enum Direction {
+    /// Write to a slave (LSB is 0)
     Write,
+    /// Read from a slave (LSB is 1)
     Read,
 }
 
@@ -163,9 +174,11 @@ pub type I2cFloating = crate::port::mode::Input<crate::port::mode::Floating>;
 #[doc(hidden)]
 pub type I2cPullUp = crate::port::mode::Input<crate::port::mode::PullUp>;
 
+/// Implement I2C traits for a TWI peripheral
 #[macro_export]
 macro_rules! impl_twi_i2c {
     (
+        $(#[$i2c_attr:meta])*
         pub struct $I2c:ident {
             peripheral: $I2C:ty,
             pins: {
@@ -189,6 +202,7 @@ macro_rules! impl_twi_i2c {
             },
         }
     ) => {
+        $(#[$i2c_attr])*
         pub struct $I2c<CLOCK: $crate::clock::Clock, M> {
             p: $I2C,
             _clock: ::core::marker::PhantomData<CLOCK>,
@@ -200,6 +214,11 @@ macro_rules! impl_twi_i2c {
         where
             CLOCK: $crate::clock::Clock,
         {
+            /// Initialize the I2C bus
+            ///
+            /// `new()` will enable the internal pull-ups to comply with the I2C
+            /// specification.  If you have external pull-ups connected, please
+            /// use `new_with_external_pullup()` instead.
             pub fn new(
                 p: $I2C,
                 sda: $sdamod::$SDA<$crate::port::mode::Input<$crate::port::mode::PullUp>>,
@@ -225,6 +244,11 @@ macro_rules! impl_twi_i2c {
         where
             CLOCK: $crate::clock::Clock,
         {
+            /// Initialize the I2C bus, without enabling internal pull-ups
+            ///
+            /// This function should be used if your hardware design includes
+            /// pull-up resistors outside the MCU.  If you do not have these,
+            /// please use `new()` instead.
             pub fn new_with_external_pullup(
                 p: $I2C,
                 sda: $sdamod::$SDA<$crate::port::mode::Input<$crate::port::mode::Floating>>,
@@ -250,6 +274,10 @@ macro_rules! impl_twi_i2c {
         where
             CLOCK: $crate::clock::Clock,
         {
+            /// Check whether a slave answers ACK for a given address
+            ///
+            /// Note that some devices might not respond to both read and write
+            /// operations.
             pub fn ping_slave(
                 &mut self,
                 addr: u8,
@@ -404,6 +432,9 @@ macro_rules! impl_twi_i2c {
             CLOCK: $crate::clock::Clock,
             $crate::delay::Delay<CLOCK>: $crate::hal::blocking::delay::DelayMs<u16>,
         {
+            /// Output an `i2cdetect`-like summary of connected slaves to a serial device
+            ///
+            /// Note that output for `Read` and `Write` might differ.
             pub fn i2cdetect<W: $crate::ufmt::uWrite>(
                 &mut self,
                 w: &mut W,

avr-hal-generic/src/lib.rs
@@ -16,6 +16,7 @@ pub mod port;
 pub mod serial;
 pub mod i2c;
 
+/// Prelude containing all HAL traits
 pub mod prelude {
     pub use hal::prelude::*;
     pub use hal::digital::v2::OutputPin as _embedded_hal_digital_v2_OutputPin;

avr-hal-generic/src/port.rs
@@ -1,10 +1,17 @@
+//! PORTx digital IO Implementations
+
+/// IO Modes
 pub mod mode {
+    /// Any digital IO mode
     pub trait DigitalIO: private::Unimplementable {}
+    /// Any input mode
     pub trait InputMode: private::Unimplementable {}
 
+    /// Pin configured as a digital input
     pub struct Input<MODE: InputMode> {
         _m: core::marker::PhantomData<MODE>,
     }
+    /// Pin configured as a digital output
     pub struct Output;
 
     impl private::Unimplementable for Output {}
@@ -12,7 +19,9 @@ pub mod mode {
     impl DigitalIO for Output {}
     impl<M: InputMode> DigitalIO for Input<M> {}
 
+    /// Pin input configured **without** internal pull-up
     pub struct Floating;
+    /// Pin input configured with internal pull-up
     pub struct PullUp;
 
     impl private::Unimplementable for Floating {}
@@ -164,6 +173,17 @@ macro_rules! impl_port {
         // Downgrade implementation ------------------------------- {{{
         $(
             impl<MODE> $portx::$PXi<MODE> {
+                /// Downgrade this pin into a type that is generic over all pins.
+                ///
+                /// The main use for this function is to store multiple pins in
+                /// an array.  Please note that generic pins have a runtime overhead.
+                ///
+                /// # Example
+                /// ```rust
+                /// let p1 = portb.pb1.downgrade();
+                /// let p2 = portc.pc7.downgrade();
+                /// let pins = [p1, p2];
+                /// ```
                 pub fn downgrade(self) -> $GenericPin<MODE> {
                     $GenericPin::$PortEnum($i, ::core::marker::PhantomData)
                 }
@@ -239,6 +259,7 @@ macro_rules! impl_port {
                     // peripheral can't be converted back (because that would
                     // not be universally possible).
 
+                    /// Make this pin a digital output
                     pub fn into_output<D: AsDDR>(self, ddr: &D) -> $PXi<mode::Output> {
                         unsafe {
                             (*<$PORTX>::ptr()).$reg_ddr.modify(|r, w| {
@@ -248,6 +269,7 @@ macro_rules! impl_port {
                         $PXi { _mode: marker::PhantomData }
                     }
 
+                    /// Make this pin a digital input **without** enabling the internal pull-up
                     pub fn into_floating_input<D: AsDDR>(self, ddr: &D) -> $PXi<mode::Input<mode::Floating>> {
                         unsafe {
                             (*<$PORTX>::ptr()).$reg_ddr.modify(|r, w| {
@@ -260,6 +282,7 @@ macro_rules! impl_port {
                         $PXi { _mode: marker::PhantomData }
                     }
 
+                    /// Make this pin a digital input and enable the internal pull-up
                     pub fn into_pull_up_input<D: AsDDR>(self, ddr: &D) -> $PXi<mode::Input<mode::PullUp>> {
                         unsafe {
                             (*<$PORTX>::ptr()).$reg_ddr.modify(|r, w| {

avr-hal-generic/src/serial.rs
@@ -1,9 +1,14 @@
+//! Serial Implementations
+
+/// Serial Error
 #[derive(Debug, Clone, Copy)]
 pub enum Error { }
 
+/// Implement serial traits for a USART peripheral
 #[macro_export]
 macro_rules! impl_usart {
     (
+        $(#[$usart_attr:meta])*
         pub struct $Usart:ident {
             peripheral: $USART:ty,
             pins: {
@@ -30,6 +35,7 @@ macro_rules! impl_usart {
             },
         }
     ) => {
+        $(#[$usart_attr])*
         pub struct $Usart<CLOCK, RX_MODE>
         where
             CLOCK: $crate::clock::Clock,
@@ -46,6 +52,11 @@ macro_rules! impl_usart {
             CLOCK: $crate::clock::Clock,
             RX_MODE: $crate::port::mode::InputMode,
         {
+            /// Initialize the USART peripheral
+            ///
+            /// Please note that not all baudrates will produce a good signal
+            /// and setting it too high might make data sent completely unreadable
+            /// for the other side.
             pub fn new(
                 p: $USART,
                 rx: $rxmod::$RX<$crate::port::mode::Input<RX_MODE>>,

chips/atmega328p-hal/src/lib.rs
@@ -14,11 +14,13 @@ pub mod prelude {
     pub use crate::port::PortExt as _atmega_PortExt;
 }
 
+/// I2C Bus
 pub mod i2c {
     use crate::port::portc;
     pub use avr_hal::i2c::*;
 
     avr_hal::impl_twi_i2c! {
+        /// I2C based on ATmega328P's TWI peripheral
         pub struct I2c {
             peripheral: crate::atmega328p::TWI,
             pins: {
@@ -44,11 +46,15 @@ pub mod i2c {
     }
 }
 
+/// Serial interface using USART
 pub mod usart {
     use crate::port::portd;
     pub use avr_hal::serial::*;
 
     crate::avr_hal::impl_usart! {
+        /// Serial interface based on ATmega328P's USART0 peripheral
+        ///
+        /// Maximum baudrate seems to be 57600
         pub struct Usart0 {
             peripheral: crate::atmega328p::USART0,
             pins: {

chips/atmega328p-hal/src/port.rs
@@ -1,3 +1,4 @@
+//! `PORTB` - `PORTD` digital IO
 pub use avr_hal::port::mode;
 
 pub trait PortExt {

commit cb2c9c94cebdc34c69c46efa9640f3c045e8a7c3

generic: Improve documentation even more
Document usage of delay with different HAL crates.

Fixes: d6daf71 ("Improve documentation")
Signed-off-by: Łukasz Stelmach <stlman@poczta.fm>

avr-hal-generic/src/delay.rs
@@ -10,7 +10,16 @@ use core::arch::asm;
 ///
 /// # Example
 /// ```rust
-/// let mut delay = delay::Delay::<clock::MHz16>::new();
+/// // Instead of arduino_hal below you may also use a different
+/// // HAL based on avr_hal_generic like attiny_hal or atmega_hal
+/// // depending on actual hardware. For example:
+/// //
+/// // use attiny_hal as hal;
+///
+/// use arduino_hal as hal;
+/// use hal::prelude::*;
+///
+/// let mut delay = hal::delay::Delay::<hal::clock::MHz16>::new();
 ///
 /// // Wait 1 second
 /// delay.delay_ms(1000);