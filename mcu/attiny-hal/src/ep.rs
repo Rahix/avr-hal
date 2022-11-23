d4e53eb600efdcce879f1e8e52b6113d7fc9c9b2
flyingyizi <flyingyizi@gmail.com>
2022-11-24 06:08:36 +0800

    rollback modification in attiny

--- mcu/attiny-hal/src/ep.rs
+++
@@ -1,6 +0,0 @@
-#[cfg(any(feature = "attiny84", feature = "attiny85", feature = "attiny167"))]
-avr_hal_generic::impl_eeprom_traditional! {Eeprom,crate::pac::EEPROM,512}
-#[cfg(any(feature = "attiny88"))]
-impl_eeprom_traditional! {Eeprom,arduino_hal::hal::pac::EEPROM,64}
-#[cfg(any(feature = "attiny2313"))]
-impl_eeprom_traditional! {Eeprom,arduino_hal::hal::pac::EEPROM,128}
--- mcu/attiny-hal/src/lib.rs
+++ mcu/attiny-hal/src/lib.rs
@@ -74,12 +74,6 @@ pub mod port;
 pub use port::Pins;
 
 #[cfg(feature = "device-selected")]
-pub mod ep;
-#[cfg(feature = "device-selected")]
-pub use ep::Eeprom;
-
-
-#[cfg(feature = "device-selected")]
 pub mod simple_pwm;
 
 pub struct Attiny;
