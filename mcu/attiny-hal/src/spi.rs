e468b7c8a6c1b181c02e2e5989aa10d728a49e16
Joshua Milas <Josh.Milas@gmail.com>
2023-05-26 08:08:23 -0400

    Commit what works

--- mcu/attiny-hal/src/spi.rs
+++ mcu/attiny-hal/src/spi.rs
@@ -39,3 +39,66 @@ avr_hal_generic::impl_spi! {
     miso: port::PA2,
     cs: port::PA6,
 }
+
+
+#[cfg(feature = "attiny85")]
+pub type Spi = avr_hal_generic::spi::Spi<
+    crate::Attiny,
+    crate::pac::USI,
+    port::PB2, //SCLK
+    port::PB0, //MOSI
+    port::PB1, //MISO
+    port::PB4, //Chip select
+    >;
+#[cfg(feature = "attiny85")]
+impl crate::spi::SpiOps<crate::Attiny, port::PB2, port::PB0, port::PB1, port::PB4> for crate::pac::USI {
+    fn raw_setup(&mut self, _settings: &Settings) {
+        self.usicr.write(|w| {
+            w.usiwm().three_wire();
+            w.usics().no_clock()
+        });
+    }
+
+    fn raw_release(&mut self) {
+        self.usicr.write(|w| {
+            w.usiwm().disabled()
+        });
+    }
+
+    fn raw_check_iflag(&self) -> bool {
+        // USIOIF is set when the counter overflows, from the datasheet
+        // ... "Can therefor be used to determine when a transfer is completed" (p 109)
+        self.usisr.read().usioif().bit_is_set()
+    }
+
+    fn raw_read(&self) -> u8 {
+        // TODO how to determine if its read fully?
+        // USIOIF tells if 8 cycles has completed, should we check first?
+        self.usibr.read().bits()
+    }
+
+    fn raw_write(&mut self, byte: u8) {
+        self.usidr.write(|w| {
+            w.bits(byte)
+        });
+
+        // Make sure the finished bit is cleared (by setting it)
+        self.usisr.write(|w| {
+            w.usioif().set_bit()
+        });
+
+        // clock bytes out
+        for _ in 0..16 {
+            self.usicr.write(|w| {
+                //w.usiwm().three_wire();
+                //w.usics().no_clock();
+                w.usiclk().set_bit();
+                w.usitc().set_bit()
+            });
+            avr_device::asm::nop();
+            avr_device::asm::nop();
+            avr_device::asm::nop();
+        }
+        // USIOIF should be set now
+    }
+}
