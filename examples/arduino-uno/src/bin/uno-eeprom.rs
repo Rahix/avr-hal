9433af70214b038e01b42680e2e88caebf937436
Flavio Vilante <fvilante1@gmail.com>
2023-06-09 03:56:06 -0300

    examples: uno: Fix EEPROM example
    The EEPROM example had a stack overflow problem due to reading the
    entire EEPROM capacity into SRAM at once.  Fix this by just reading less
    data in the example program.

--- examples/arduino-uno/src/bin/uno-eeprom.rs
+++ examples/arduino-uno/src/bin/uno-eeprom.rs
@@ -17,7 +17,10 @@ fn main() -> ! {
     let ep_capacity = ep.capacity();
     ufmt::uwriteln!(&mut serial, "eeprom capacity is:{}\r", ep_capacity).void_unwrap();
 
-    let mut data = [0_u8; arduino_hal::Eeprom::CAPACITY as usize];
+    // KNOWN ISSUE: Avoid to read entire eeprom capacity at once
+    // See: https://github.com/Rahix/avr-hal/issues/410
+    let mut data = [0_u8; 10];
+    
     let _start_address: u16 = 0;
 
     if ep.read(0, &mut data).is_err() {
