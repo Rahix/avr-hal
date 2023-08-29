f2c7fda56ffd67869dc522b8af1643a9689e2689
Fundevoge <59122389+Fundevoge@users.noreply.github.com>
2023-08-29 22:32:16 +0200

    ravedude: Modify serial port buffer size for windows to 1

--- ravedude/src/console.rs
+++ ravedude/src/console.rs
@@ -21,7 +21,13 @@ pub fn open(port: &std::path::Path, baudrate: u32) -> anyhow::Result<()> {
 
     // Spawn a thread for the receiving end because stdio is not portably non-blocking...
     std::thread::spawn(move || loop {
+        #[cfg(not(target_os = "windows"))]
         let mut buf = [0u8; 4098];
+
+        // Use buffer size 1 for windows because it blocks on rx.read until the buffer is full
+        #[cfg(target_os = "windows")]
+        let mut buf = [0u8; 1];
+        
         match rx.read(&mut buf) {
             Ok(count) => {
                 stdout.write(&buf[..count]).unwrap();
