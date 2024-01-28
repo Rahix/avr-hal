Rahix/avr-hal

commit 063f845a0940a865c591d2156cd0a8267e2346a8

generic: simplify port macro via `paste!`

avr-hal-generic/src/port.rs
@@ -463,15 +463,9 @@ impl<PIN: PinOps> Pin<mode::Analog, PIN> {
 #[macro_export]
 macro_rules! impl_port_traditional {
     (
-        enum Ports {
-            $($PortName:ident: ($Port:ty, $port_port_reg:ident, $port_pin_reg:ident, $port_ddr_reg:ident),)+
-        }
-
         $(#[$pins_attr:meta])*
-        pub struct Pins {
-            $($pin:ident: $Pin:ident = ($PinPort:ty, $PinPortName:ident, $pin_num:expr,
-                                        $pin_port_reg:ident, $pin_pin_reg:ident,
-                                        $pin_ddr_reg:ident),)+
+        enum Ports {
+            $($name:ident: $port:ty = [$($pin:literal),+],)+
         }
     ) => {
         /// Type-alias for a pin type which can represent any concrete pin.
@@ -482,29 +476,33 @@ macro_rules! impl_port_traditional {
         /// "dynamic" type.  Do note, however, that using this dynamic type has a runtime cost.
         pub type Pin<MODE, PIN = Dynamic> = $crate::port::Pin<MODE, PIN>;
 
-        $(#[$pins_attr])*
-        pub struct Pins {
-            $(pub $pin: Pin<
-                mode::Input<mode::Floating>,
-                $Pin,
-            >,)+
-        }
+        $crate::paste::paste! {
+            $(#[$pins_attr])*
+            pub struct Pins {
+                $($(pub [<p $name:lower $pin>]: Pin<
+                    mode::Input<mode::Floating>,
+                    [<P $name $pin>],
+                >,)+)+
+            }
 
-        impl Pins {
-            pub fn new(
-                $(_: $Port,)+
-            ) -> Self {
-                Self {
-                    $($pin: $crate::port::Pin::new(
-                        $Pin { _private: (), }
-                    ),)+
+            impl Pins {
+                pub fn new(
+                    $(_: $port,)+
+                ) -> Self {
+                    Self {
+                        $($([<p $name:lower $pin>]: $crate::port::Pin::new(
+                            [<P $name $pin>] { _private: (), }
+                        ),)+)+
+                    }
                 }
             }
         }
 
-        #[repr(u8)]
-        pub enum DynamicPort {
-            $($PortName,)+
+        $crate::paste::paste! {
+            #[repr(u8)]
+            pub enum DynamicPort {
+                $([<PORT $name>]),+
+            }
         }
 
         pub struct Dynamic {
@@ -523,135 +521,74 @@ macro_rules! impl_port_traditional {
             }
         }
 
-        impl $crate::port::PinOps for Dynamic {
-            type Dynamic = Self;
-
-            #[inline]
-            fn into_dynamic(self) -> Self::Dynamic {
-                self
-            }
-
-            #[inline]
-            unsafe fn out_set(&mut self) {
-                match self.port {
-                    $(DynamicPort::$PortName => (*<$Port>::ptr()).$port_port_reg.modify(|r, w| {
-                        w.bits(r.bits() | self.mask)
-                    }),)+
-                }
-            }
-
-            #[inline]
-            unsafe fn out_clear(&mut self) {
-                match self.port {
-                    $(DynamicPort::$PortName => (*<$Port>::ptr()).$port_port_reg.modify(|r, w| {
-                        w.bits(r.bits() & !self.mask)
-                    }),)+
-                }
-            }
-
-            #[inline]
-            unsafe fn out_toggle(&mut self) {
-                match self.port {
-                    $(DynamicPort::$PortName => (*<$Port>::ptr()).$port_pin_reg.write(|w| {
-                        w.bits(self.mask)
-                    }),)+
-                }
-            }
-
-            #[inline]
-            unsafe fn out_get(&self) -> bool {
-                match self.port {
-                    $(DynamicPort::$PortName => (*<$Port>::ptr()).$port_port_reg.read().bits()
-                        & self.mask != 0,)+
-                }
-            }
-
-            #[inline]
-            unsafe fn in_get(&self) -> bool {
-                match self.port {
-                    $(DynamicPort::$PortName => (*<$Port>::ptr()).$port_pin_reg.read().bits()
-                        & self.mask != 0,)+
-                }
-            }
-
-            #[inline]
-            unsafe fn make_output(&mut self) {
-                match self.port {
-                    $(DynamicPort::$PortName => (*<$Port>::ptr()).$port_ddr_reg.modify(|r, w| {
-                        w.bits(r.bits() | self.mask)
-                    }),)+
-                }
-            }
-
-            #[inline]
-            unsafe fn make_input(&mut self, pull_up: bool) {
-                match self.port {
-                    $(DynamicPort::$PortName => (*<$Port>::ptr()).$port_ddr_reg.modify(|r, w| {
-                        w.bits(r.bits() & !self.mask)
-                    }),)+
-                }
-                if pull_up {
-                    self.out_set()
-                } else {
-                    self.out_clear()
-                }
-            }
-        }
-
-        $(
-            pub struct $Pin {
-                _private: ()
-            }
-
-            impl $crate::port::PinOps for $Pin {
-                type Dynamic = Dynamic;
+        $crate::paste::paste! {
+            impl $crate::port::PinOps for Dynamic {
+                type Dynamic = Self;
 
                 #[inline]
                 fn into_dynamic(self) -> Self::Dynamic {
-                    Dynamic::new(DynamicPort::$PinPortName, $pin_num)
+                    self
                 }
 
                 #[inline]
                 unsafe fn out_set(&mut self) {
-                    (*<$PinPort>::ptr()).$pin_port_reg.modify(|r, w| {
-                        w.bits(r.bits() | (1 << $pin_num))
-                    })
+                    match self.port {
+                        $(DynamicPort::[<PORT $name>] => (*<$port>::ptr()).[<port $name:lower>].modify(|r, w| {
+                            w.bits(r.bits() | self.mask)
+                        }),)+
+                    }
                 }
 
                 #[inline]
                 unsafe fn out_clear(&mut self) {
-                    (*<$PinPort>::ptr()).$pin_port_reg.modify(|r, w| {
-                        w.bits(r.bits() & !(1 << $pin_num))
-                    })
+                    match self.port {
+                        $(DynamicPort::[<PORT $name>] => (*<$port>::ptr()).[<port $name:lower>].modify(|r, w| {
+                            w.bits(r.bits() & !self.mask)
+                        }),)+
+                    }
                 }
 
                 #[inline]
                 unsafe fn out_toggle(&mut self) {
-                    (*<$PinPort>::ptr()).$pin_pin_reg.write(|w| w.bits(1 << $pin_num))
+                    match self.port {
+                        $(DynamicPort::[<PORT $name>] => (*<$port>::ptr()).[<port $name:lower>].write(|w| {
+                            w.bits(self.mask)
+                        }),)+
+                    }
                 }
 
                 #[inline]
                 unsafe fn out_get(&self) -> bool {
-                    (*<$PinPort>::ptr()).$pin_port_reg.read().bits() & (1 << $pin_num) != 0
+                    match self.port {
+                        $(DynamicPort::[<PORT $name>] => (*<$port>::ptr()).[<port $name:lower>].read().bits()
+                            & self.mask != 0,)+
+                    }
                 }
 
                 #[inline]
                 unsafe fn in_get(&self) -> bool {
-                    (*<$PinPort>::ptr()).$pin_pin_reg.read().bits() & (1 << $pin_num) != 0
+                    match self.port {
+                        $(DynamicPort::[<PORT $name>] => (*<$port>::ptr()).[<pin $name:lower>].read().bits()
+                            & self.mask != 0,)+
+                    }
                 }
 
                 #[inline]
                 unsafe fn make_output(&mut self) {
-                    (*<$PinPort>::ptr()).$pin_ddr_reg.modify(|r, w| {
-                        w.bits(r.bits() | (1 << $pin_num))
-                    })
+                    match self.port {
+                        $(DynamicPort::[<PORT $name>] => (*<$port>::ptr()).[<ddr $name:lower>].modify(|r, w| {
+                            w.bits(r.bits() | self.mask)
+                        }),)+
+                    }
                 }
 
                 #[inline]
                 unsafe fn make_input(&mut self, pull_up: bool) {
-                    (*<$PinPort>::ptr()).$pin_ddr_reg.modify(|r, w| {
-                        w.bits(r.bits() & !(1 << $pin_num))
-                    });
+                    match self.port {
+                        $(DynamicPort::[<PORT $name>] => (*<$port>::ptr()).[<ddr $name:lower>].modify(|r, w| {
+                            w.bits(r.bits() & !self.mask)
+                        }),)+
+                    }
                     if pull_up {
                         self.out_set()
                     } else {
@@ -659,33 +596,101 @@ macro_rules! impl_port_traditional {
                     }
                 }
             }
-        )+
+        }
+
+        $crate::paste::paste! {
+            $($(
+                pub struct [<P $name $pin>] {
+                    _private: ()
+                }
+
+                impl $crate::port::PinOps for [<P $name $pin>] {
+                    type Dynamic = Dynamic;
+
+                    #[inline]
+                    fn into_dynamic(self) -> Self::Dynamic {
+                        Dynamic::new(DynamicPort::[<PORT $name>], $pin)
+                    }
+
+                    #[inline]
+                    unsafe fn out_set(&mut self) {
+                        (*<$port>::ptr()).[<port $name:lower>].modify(|r, w| {
+                            w.bits(r.bits() | (1 << $pin))
+                        })
+                    }
+
+                    #[inline]
+                    unsafe fn out_clear(&mut self) {
+                        (*<$port>::ptr()).[<port $name:lower>].modify(|r, w| {
+                            w.bits(r.bits() & !(1 << $pin))
+                        })
+                    }
+
+                    #[inline]
+                    unsafe fn out_toggle(&mut self) {
+                        (*<$port>::ptr()).[<pin $name:lower>].write(|w| w.bits(1 << $pin))
+                    }
+
+                    #[inline]
+                    unsafe fn out_get(&self) -> bool {
+                        (*<$port>::ptr()).[<port $name:lower>].read().bits() & (1 << $pin) != 0
+                    }
+
+                    #[inline]
+                    unsafe fn in_get(&self) -> bool {
+                        (*<$port>::ptr()).[<pin $name:lower>].read().bits() & (1 << $pin) != 0
+                    }
+
+                    #[inline]
+                    unsafe fn make_output(&mut self) {
+                        (*<$port>::ptr()).[<ddr $name:lower>].modify(|r, w| {
+                            w.bits(r.bits() | (1 << $pin))
+                        })
+                    }
+
+                    #[inline]
+                    unsafe fn make_input(&mut self, pull_up: bool) {
+                        (*<$port>::ptr()).[<ddr $name:lower>].modify(|r, w| {
+                            w.bits(r.bits() & !(1 << $pin))
+                        });
+                        if pull_up {
+                            self.out_set()
+                        } else {
+                            self.out_clear()
+                        }
+                    }
+                }
+            )+)+
+        }
     };
 }
 
 #[macro_export]
 macro_rules! renamed_pins {
     (
-        type Pin = $PinType:ident;
-
         $(#[$pins_attr:meta])*
-        pub struct Pins from $McuPins:ty {
-            $($(#[$pin_attr:meta])* pub $pin:ident: $Pin:ty = $pin_orig:ident,)+
+        pub struct Pins {
+            $($(#[$pin_attr:meta])* pub $pin_name:ident: $pin_type:ty = $pin_orig:ident,)+
+        }
+
+        impl Pins {
+            type Pin = $pin_wrapper:ident;
+            type McuPins = $mcu_pins:ty;
         }
     ) => {
         $(#[$pins_attr])*
         pub struct Pins {
             $($(#[$pin_attr])*
-            pub $pin: $PinType<
+            pub $pin_name: $pin_wrapper<
                 $crate::port::mode::Input<$crate::port::mode::Floating>,
-                $Pin,
+                $pin_type,
             >,)+
         }
 
         impl Pins {
-            pub fn with_mcu_pins(pins: $McuPins) -> Self {
+            pub fn with_mcu_pins(pins: $mcu_pins) -> Self {
                 Self {
-                    $($pin: pins.$pin_orig,)+
+                    $($pin_name: pins.$pin_orig,)+
                 }
             }
         }

mcu/attiny-hal/src/port.rs
@@ -1,138 +1,43 @@
-pub use avr_hal_generic::port::{mode, PinOps, PinMode};
+pub use avr_hal_generic::port::{mode, PinMode, PinOps};
 
 #[cfg(feature = "attiny2313")]
 avr_hal_generic::impl_port_traditional! {
     enum Ports {
-        PORTA: (crate::pac::PORTA, porta, pina, ddra),
-        PORTB: (crate::pac::PORTB, portb, pinb, ddrb),
-        PORTD: (crate::pac::PORTD, portd, pind, ddrd),
-    }
-
-    pub struct Pins {
-        pa0: PA0 = (crate::pac::PORTA, PORTA, 0, porta, pina, ddra),
-        pa1: PA1 = (crate::pac::PORTA, PORTA, 1, porta, pina, ddra),
-        pa2: PA2 = (crate::pac::PORTA, PORTA, 2, porta, pina, ddra),
-        pb0: PB0 = (crate::pac::PORTB, PORTB, 0, portb, pinb, ddrb),
-        pb1: PB1 = (crate::pac::PORTB, PORTB, 1, portb, pinb, ddrb),
-        pb2: PB2 = (crate::pac::PORTB, PORTB, 2, portb, pinb, ddrb),
-        pb3: PB3 = (crate::pac::PORTB, PORTB, 3, portb, pinb, ddrb),
-        pb4: PB4 = (crate::pac::PORTB, PORTB, 4, portb, pinb, ddrb),
-        pb5: PB5 = (crate::pac::PORTB, PORTB, 5, portb, pinb, ddrb),
-        pb6: PB6 = (crate::pac::PORTB, PORTB, 6, portb, pinb, ddrb),
-        pb7: PB7 = (crate::pac::PORTB, PORTB, 7, portb, pinb, ddrb),
-        pd0: PD0 = (crate::pac::PORTD, PORTD, 0, portd, pind, ddrd),
-        pd1: PD1 = (crate::pac::PORTD, PORTD, 1, portd, pind, ddrd),
-        pd2: PD2 = (crate::pac::PORTD, PORTD, 2, portd, pind, ddrd),
-        pd3: PD3 = (crate::pac::PORTD, PORTD, 3, portd, pind, ddrd),
-        pd4: PD4 = (crate::pac::PORTD, PORTD, 4, portd, pind, ddrd),
-        pd5: PD5 = (crate::pac::PORTD, PORTD, 5, portd, pind, ddrd),
-        pd6: PD6 = (crate::pac::PORTD, PORTD, 6, portd, pind, ddrd),
+        A: crate::pac::PORTA = [0, 1, 2],
+        B: crate::pac::PORTB = [0, 1, 2, 3, 4, 5, 6, 7],
+        D: crate::pac::PORTD = [0, 1, 2, 3, 4, 5, 6],
     }
 }
 
 #[cfg(feature = "attiny167")]
 avr_hal_generic::impl_port_traditional! {
     enum Ports {
-        PORTA: (crate::pac::PORTA, porta, pina, ddra),
-        PORTB: (crate::pac::PORTB, portb, pinb, ddrb),
-    }
-
-    pub struct Pins {
-        pa0: PA0 = (crate::pac::PORTA, PORTA, 0, porta, pina, ddra),
-        pa1: PA1 = (crate::pac::PORTA, PORTA, 1, porta, pina, ddra),
-        pa2: PA2 = (crate::pac::PORTA, PORTA, 2, porta, pina, ddra),
-        pa3: PA3 = (crate::pac::PORTA, PORTA, 3, porta, pina, ddra),
-        pa4: PA4 = (crate::pac::PORTA, PORTA, 4, porta, pina, ddra),
-        pa5: PA5 = (crate::pac::PORTA, PORTA, 5, porta, pina, ddra),
-        pa6: PA6 = (crate::pac::PORTA, PORTA, 6, porta, pina, ddra),
-        pa7: PA7 = (crate::pac::PORTA, PORTA, 7, porta, pina, ddra),
-        pb0: PB0 = (crate::pac::PORTB, PORTB, 0, portb, pinb, ddrb),
-        pb1: PB1 = (crate::pac::PORTB, PORTB, 1, portb, pinb, ddrb),
-        pb2: PB2 = (crate::pac::PORTB, PORTB, 2, portb, pinb, ddrb),
-        pb3: PB3 = (crate::pac::PORTB, PORTB, 3, portb, pinb, ddrb),
-        pb4: PB4 = (crate::pac::PORTB, PORTB, 4, portb, pinb, ddrb),
-        pb5: PB5 = (crate::pac::PORTB, PORTB, 5, portb, pinb, ddrb),
-        pb6: PB6 = (crate::pac::PORTB, PORTB, 6, portb, pinb, ddrb),
-        pb7: PB7 = (crate::pac::PORTB, PORTB, 7, portb, pinb, ddrb),
+        A: crate::pac::PORTA = [0, 1, 2, 3, 4, 5, 6, 7],
+        B: crate::pac::PORTB = [0, 1, 2, 3, 4, 5, 6, 7],
     }
 }
 
 #[cfg(feature = "attiny84")]
 avr_hal_generic::impl_port_traditional! {
     enum Ports {
-        PORTA: (crate::pac::PORTA, porta, pina, ddra),
-        PORTB: (crate::pac::PORTB, portb, pinb, ddrb),
-    }
-
-    pub struct Pins {
-        pa0: PA0 = (crate::pac::PORTA, PORTA, 0, porta, pina, ddra),
-        pa1: PA1 = (crate::pac::PORTA, PORTA, 1, porta, pina, ddra),
-        pa2: PA2 = (crate::pac::PORTA, PORTA, 2, porta, pina, ddra),
-        pa3: PA3 = (crate::pac::PORTA, PORTA, 3, porta, pina, ddra),
-        pa4: PA4 = (crate::pac::PORTA, PORTA, 4, porta, pina, ddra),
-        pa5: PA5 = (crate::pac::PORTA, PORTA, 5, porta, pina, ddra),
-        pa6: PA6 = (crate::pac::PORTA, PORTA, 6, porta, pina, ddra),
-        pa7: PA7 = (crate::pac::PORTA, PORTA, 7, porta, pina, ddra),
-        pb0: PB0 = (crate::pac::PORTB, PORTB, 0, portb, pinb, ddrb),
-        pb1: PB1 = (crate::pac::PORTB, PORTB, 1, portb, pinb, ddrb),
-        pb2: PB2 = (crate::pac::PORTB, PORTB, 2, portb, pinb, ddrb),
-        pb3: PB3 = (crate::pac::PORTB, PORTB, 3, portb, pinb, ddrb),
+        A: crate::pac::PORTA = [0, 1, 2, 3, 4, 5, 6, 7],
+        B: crate::pac::PORTB = [0, 1, 2, 3],
     }
 }
 
 #[cfg(feature = "attiny85")]
 avr_hal_generic::impl_port_traditional! {
     enum Ports {
-        PORTB: (crate::pac::PORTB, portb, pinb, ddrb),
-    }
-
-    pub struct Pins {
-        pb0: PB0 = (crate::pac::PORTB, PORTB, 0, portb, pinb, ddrb),
-        pb1: PB1 = (crate::pac::PORTB, PORTB, 1, portb, pinb, ddrb),
-        pb2: PB2 = (crate::pac::PORTB, PORTB, 2, portb, pinb, ddrb),
-        pb3: PB3 = (crate::pac::PORTB, PORTB, 3, portb, pinb, ddrb),
-        pb4: PB4 = (crate::pac::PORTB, PORTB, 4, portb, pinb, ddrb),
-        pb5: PB5 = (crate::pac::PORTB, PORTB, 5, portb, pinb, ddrb),
+        B: crate::pac::PORTB = [0, 1, 2, 3, 4, 5],
     }
 }
 
 #[cfg(feature = "attiny88")]
 avr_hal_generic::impl_port_traditional! {
     enum Ports {
-        PORTA: (crate::pac::PORTA, porta, pina, ddra),
-        PORTB: (crate::pac::PORTB, portb, pinb, ddrb),
-        PORTC: (crate::pac::PORTC, portc, pinc, ddrc),
-        PORTD: (crate::pac::PORTD, portd, pind, ddrd),
-    }
-
-    pub struct Pins {
-        pa0: PA0 = (crate::pac::PORTA, PORTA, 0, porta, pina, ddra),
-        pa1: PA1 = (crate::pac::PORTA, PORTA, 1, porta, pina, ddra),
-        pa2: PA2 = (crate::pac::PORTA, PORTA, 2, porta, pina, ddra),
-        pa3: PA3 = (crate::pac::PORTA, PORTA, 3, porta, pina, ddra),
-        pb0: PB0 = (crate::pac::PORTB, PORTB, 0, portb, pinb, ddrb),
-        pb1: PB1 = (crate::pac::PORTB, PORTB, 1, portb, pinb, ddrb),
-        pb2: PB2 = (crate::pac::PORTB, PORTB, 2, portb, pinb, ddrb),
-        pb3: PB3 = (crate::pac::PORTB, PORTB, 3, portb, pinb, ddrb),
-        pb4: PB4 = (crate::pac::PORTB, PORTB, 4, portb, pinb, ddrb),
-        pb5: PB5 = (crate::pac::PORTB, PORTB, 5, portb, pinb, ddrb),
-        pb6: PB6 = (crate::pac::PORTB, PORTB, 6, portb, pinb, ddrb),
-        pb7: PB7 = (crate::pac::PORTB, PORTB, 7, portb, pinb, ddrb),
-        pc0: PC0 = (crate::pac::PORTC, PORTC, 0, portc, pinc, ddrc),
-        pc1: PC1 = (crate::pac::PORTC, PORTC, 1, portc, pinc, ddrc),
-        pc2: PC2 = (crate::pac::PORTC, PORTC, 2, portc, pinc, ddrc),
-        pc3: PC3 = (crate::pac::PORTC, PORTC, 3, portc, pinc, ddrc),
-        pc4: PC4 = (crate::pac::PORTC, PORTC, 4, portc, pinc, ddrc),
-        pc5: PC5 = (crate::pac::PORTC, PORTC, 5, portc, pinc, ddrc),
-        pc6: PC6 = (crate::pac::PORTC, PORTC, 6, portc, pinc, ddrc),
-        pc7: PC7 = (crate::pac::PORTC, PORTC, 7, portc, pinc, ddrc),
-        pd0: PD0 = (crate::pac::PORTD, PORTD, 0, portd, pind, ddrd),
-        pd1: PD1 = (crate::pac::PORTD, PORTD, 1, portd, pind, ddrd),
-        pd2: PD2 = (crate::pac::PORTD, PORTD, 2, portd, pind, ddrd),
-        pd3: PD3 = (crate::pac::PORTD, PORTD, 3, portd, pind, ddrd),
-        pd4: PD4 = (crate::pac::PORTD, PORTD, 4, portd, pind, ddrd),
-        pd5: PD5 = (crate::pac::PORTD, PORTD, 5, portd, pind, ddrd),
-        pd6: PD6 = (crate::pac::PORTD, PORTD, 6, portd, pind, ddrd),
-        pd7: PD7 = (crate::pac::PORTD, PORTD, 7, portd, pind, ddrd),
+        A: crate::pac::PORTA = [0, 1, 2, 3],
+        B: crate::pac::PORTB = [0, 1, 2, 3, 4, 5, 6, 7],
+        C: crate::pac::PORTC = [0, 1, 2, 3, 4, 5, 6, 7],
+        D: crate::pac::PORTD = [0, 1, 2, 3, 4, 5, 6, 7],
     }
 }

arduino-hal/src/port/diecimila.rs
@@ -1,12 +1,10 @@
-pub use atmega_hal::port::{mode, Pin, PinOps, PinMode};
+pub use atmega_hal::port::{mode, Pin, PinMode, PinOps};
 
 avr_hal_generic::renamed_pins! {
-    type Pin = Pin;
-
     /// Pins of the **Arduino Diecimila**.
     ///
     /// This struct is best initialized via the [`arduino_hal::pins!()`][crate::pins] macro.
-    pub struct Pins from atmega_hal::Pins {
+    pub struct Pins {
         /// `A0`
         ///
         /// * ADC0 (ADC input channel 0)
@@ -124,5 +122,10 @@ avr_hal_generic::renamed_pins! {
         /// * PCINT5 (pin change interrupt 5)
         /// * L LED on Arduino Uno
         pub d13: atmega_hal::port::PB5 = pb5,
+    }
+
+    impl Pins {
+        type Pin = Pin;
+        type McuPins = atmega_hal::Pins;
     }
 }

arduino-hal/src/port/leonardo.rs
@@ -1,12 +1,10 @@
-pub use atmega_hal::port::{mode, Pin, PinOps, PinMode};
+pub use atmega_hal::port::{mode, Pin, PinMode, PinOps};
 
 avr_hal_generic::renamed_pins! {
-    type Pin = Pin;
-
     /// Pins of the **Arduino Leonardo**.
     ///
     /// This struct is best initialized via the [`arduino_hal::pins!()`][crate::pins] macro.
-    pub struct Pins from atmega_hal::Pins {
+    pub struct Pins {
         /// `D0` / `RX`
         ///
         /// * `RX` (UART)
@@ -120,5 +118,10 @@ avr_hal_generic::renamed_pins! {
         ///
         /// * `ADC0` channel
         pub a5: atmega_hal::port::PF0 = pf0,
+    }
+
+    impl Pins {
+        type Pin = Pin;
+        type McuPins = atmega_hal::Pins;
     }
 }

arduino-hal/src/port/mega.rs
@@ -1,17 +1,15 @@
-pub use atmega_hal::port::{mode, Pin, PinOps, PinMode};
+pub use atmega_hal::port::{mode, Pin, PinMode, PinOps};
 
 avr_hal_generic::renamed_pins! {
-    type Pin = Pin;
-
     /// Pins of the **Arduino Mega 2560** and **Arduino Mega 1280**.
-    /// 
+    ///
     /// mega1280:
     ///     https://www.arduino.cc/en/uploads/Main/arduino-mega-schematic.pdf
     /// mega2560:
     ///     https://www.arduino.cc/en/uploads/Main/arduino-mega2560-schematic.pdf
-    /// 
+    ///
     /// This struct is best initialized via the [`arduino_hal::pins!()`][crate::pins] macro.
-    pub struct Pins from atmega_hal::Pins {
+    pub struct Pins {
         /// `D0` / `RX0`
         ///
         /// * `RXD0` (USART0)
@@ -336,5 +334,10 @@ avr_hal_generic::renamed_pins! {
         /// * `ADC15`: A/D converter input 15
         /// * `PCINT23`: External Interrupt (Pin Change)
         pub a15: atmega_hal::port::PK7 = pk7,
+    }
+
+    impl Pins {
+        type Pin = Pin;
+        type McuPins = atmega_hal::Pins;
     }
 }

arduino-hal/src/port/promicro.rs
@@ -1,12 +1,10 @@
-pub use atmega_hal::port::{mode, Pin, PinOps, PinMode};
+pub use atmega_hal::port::{mode, Pin, PinMode, PinOps};
 
 avr_hal_generic::renamed_pins! {
-    type Pin = Pin;
-
     /// Pins of the **SparkFun ProMicro**.
     ///
     /// This struct is best initialized via the [`arduino_hal::pins!()`][crate::pins] macro.
-    pub struct Pins from atmega_hal::Pins {
+    pub struct Pins {
         /// `RX`
         ///
         /// `RX` (UART)
@@ -73,5 +71,10 @@ avr_hal_generic::renamed_pins! {
         ///
         /// `ADC4` channel
         pub a3: atmega_hal::port::PF4 = pf4,
+    }
+
+    impl Pins {
+        type Pin = Pin;
+        type McuPins = atmega_hal::Pins;
     }
 }

arduino-hal/src/port/trinket.rs
@@ -1,18 +1,21 @@
-pub use attiny_hal::port::{mode, Pin, PinOps, PinMode};
+pub use attiny_hal::port::{mode, Pin, PinMode, PinOps};
 
 avr_hal_generic::renamed_pins! {
-    type Pin = Pin;
+    pub struct Pins {
+        /// `#0`: `PB0`, `DI`(SPI), `SDA`(I2C)
+        pub d0: attiny_hal::port::PB0 = pb0,
+        /// `#1`: `PB1`, `DO`(SPI), Builtin LED
+        pub d1: attiny_hal::port::PB1 = pb1,
+        /// `#2`: `PB2`, `SCK`(SPI), `SCL`(I2C)
+        pub d2: attiny_hal::port::PB2 = pb2,
+        /// `#3`: `PB3`
+        pub d3: attiny_hal::port::PB3 = pb3,
+        /// `#4`: `PB4`
+        pub d4: attiny_hal::port::PB4 = pb4,
+    }
 
-    pub struct Pins from attiny_hal::Pins {
-     /// `#0`: `PB0`, `DI`(SPI), `SDA`(I2C)
-     pub d0: attiny_hal::port::PB0 = pb0,
-     /// `#1`: `PB1`, `DO`(SPI), Builtin LED
-     pub d1: attiny_hal::port::PB1 = pb1,
-     /// `#2`: `PB2`, `SCK`(SPI), `SCL`(I2C)
-     pub d2: attiny_hal::port::PB2 = pb2,
-     /// `#3`: `PB3`
-     pub d3: attiny_hal::port::PB3 = pb3,
-     /// `#4`: `PB4`
-     pub d4: attiny_hal::port::PB4 = pb4,
+    impl Pins {
+        type Pin = Pin;
+        type McuPins = attiny_hal::Pins;
     }
 }

commit 6efc8081e4af46f37d078e36c865a0ca88ae9ace

generic: port: Fix toggle() for dynamic pins
During commit 063f845a0940 ("generic: simplify port macro via
`paste!`"), an accidental change was introduced where out_toggle() for
dynamic pins wrote to the `PORT` register instead of the `PIN` register.
This meant that toggle() for dynamic pins no longer worked.

Fix this regression by making out_toggle() write to `PIN` again.

Fixes: 063f845a0940 ("generic: simplify port macro via `paste!`")

avr-hal-generic/src/port.rs
@@ -644,7 +644,7 @@ macro_rules! impl_port_traditional {
                 #[inline]
                 unsafe fn out_toggle(&mut self) {
                     match self.port {
-                        $(DynamicPort::[<PORT $name>] => (*<$port>::ptr()).[<port $name:lower>].write(|w| {
+                        $(DynamicPort::[<PORT $name>] => (*<$port>::ptr()).[<pin $name:lower>].write(|w| {
                             w.bits(self.mask)
                         }),)+
                     }