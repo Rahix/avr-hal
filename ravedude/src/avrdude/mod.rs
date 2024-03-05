Rahix/avr-hal

commit 8137707b8783d2bee77f2cec4ff82e4dc0fd02e9

ravedude: Use a different avrdude config for avrdude 7.X
The config we've been shipping so far was only for avrdude version 6.X.
This config does not work when being used with avrdude 7.X versions.

As we do not have control over the installed avrdude version at this
time, we need to provide different configurations depending on the
avrdude version we find.

As a stop-gap solution, import the released default config for avrdude
7.3 and attempt using it for all avrdude 7.X versions.

In the future, we'll have to rethink this.  It is probably a much better
solution to simply vendor avrdude entirely.

ravedude/src/avrdude/mod.rs
@@ -52,6 +52,8 @@ impl Avrdude {
         port: Option<impl AsRef<path::Path>>,
         bin: &path::Path,
     ) -> anyhow::Result<Self> {
+        let avrdude_version = Self::get_avrdude_version()?;
+
         let config = tempfile::Builder::new()
             .prefix(".avrdude-")
             .suffix(".conf")
@@ -62,8 +64,13 @@ impl Avrdude {
             use std::io::Write;
 
             let mut f = std::fs::File::create(&config).context("could not create avrdude.conf")?;
-            f.write_all(include_bytes!("avrdude.conf"))
-                .context("could not write avrdude.conf")?;
+            if avrdude_version.0 == 6 {
+                f.write_all(include_bytes!("avrdude-6.conf"))
+                    .context("could not write avrdude.conf for avrdude 6.X")?;
+            } else {
+                f.write_all(include_bytes!("avrdude-7.conf"))
+                    .context("could not write avrdude.conf for avrdude 7.X")?;
+            }
             f.flush().unwrap();
         }
 

commit 09a712c1ad7a59d7e35d972ed3680dc139079c2c

ravedude: Fix support for avrdude 7.0 - 7.2
The changes in 8137707b8783 ("ravedude: Use a different avrdude config
for avrdude 7.X") were unfortunately too quick because they break
against avrdude versions 7.0 up to 7.2.

Because our avrdude 7.X config matches the upstream file right now, stop
vendoring the config for newer versions, starting from 7.1.  This is a
stop-gap solution yet again, as we will most likely need to tweak
avrdude configs for the newer versions again at some point.

Fixes: 8137707b8783 ("ravedude: Use a different avrdude config for avrdude 7.X")

ravedude/src/avrdude/mod.rs
@@ -2,6 +2,8 @@ use anyhow::Context as _;
 use std::path;
 use std::process;
 
+use std::io::Write;
+
 #[derive(Debug)]
 pub struct AvrdudeOptions<'a> {
     pub programmer: &'a str,
@@ -13,7 +15,7 @@ pub struct AvrdudeOptions<'a> {
 #[derive(Debug)]
 pub struct Avrdude {
     #[allow(dead_code)]
-    config: tempfile::NamedTempFile,
+    config: Option<tempfile::NamedTempFile>,
     process: process::Child,
 }
 
@@ -54,30 +56,27 @@ impl Avrdude {
     ) -> anyhow::Result<Self> {
         let avrdude_version = Self::get_avrdude_version()?;
 
-        let config = tempfile::Builder::new()
-            .prefix(".avrdude-")
-            .suffix(".conf")
-            .tempfile()
-            .unwrap();
+        let mut command = &mut process::Command::new("avrdude");
 
-        {
-            use std::io::Write;
-
+        let config = if avrdude_version <= (7, 0) {
+            let config = tempfile::Builder::new()
+                .prefix(".avrdude-")
+                .suffix(".conf")
+                .tempfile()
+                .unwrap();
             let mut f = std::fs::File::create(&config).context("could not create avrdude.conf")?;
-            if avrdude_version.0 == 6 {
-                f.write_all(include_bytes!("avrdude-6.conf"))
-                    .context("could not write avrdude.conf for avrdude 6.X")?;
-            } else {
-                f.write_all(include_bytes!("avrdude-7.conf"))
-                    .context("could not write avrdude.conf for avrdude 7.X")?;
-            }
+            f.write_all(include_bytes!("avrdude-6.conf"))
+                .context("could not write avrdude.conf for avrdude <=7.0")?;
             f.flush().unwrap();
-        }
 
-        let mut command = process::Command::new("avrdude");
+            command = command.arg("-C").arg(&config.as_ref());
+            Some(config)
+        } else {
+            // For avrdude versions >=7.1, we don't supply a custom configuration file for now.
+            None
+        };
+
         let mut command = command
-            .arg("-C")
-            .arg(&config.as_ref())
             .arg("-c")
             .arg(options.programmer)
             .arg("-p")