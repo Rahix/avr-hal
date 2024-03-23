6fa3aee3faff31441eb2f516fb57ea528440151c
Armandas Jarušauskas <jarusauskas@gmail.com>
2024-03-23 11:49:59 +0900

    Update atmega2560-adc.rs

--- examples/atmega2560/src/bin/atmega2560-adc.rs
+++ examples/atmega2560/src/bin/atmega2560-adc.rs
@@ -1,53 +1,66 @@
 #![no_std]
 #![no_main]
 
-use arduino_hal::prelude::*;
+use atmega_hal::adc::channel;
+use atmega_hal::clock;
+use atmega_hal::delay::Delay;
+use atmega_hal::usart::{Baudrate, Usart};
+use embedded_hal::delay::DelayNs;
 use panic_halt as _;
 
-use arduino_hal::adc;
+type Adc = atmega_hal::adc::Adc<clock::MHz16>;
 
-#[arduino_hal::entry]
+#[avr_device::entry]
 fn main() -> ! {
-    let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = arduino_hal::pins!(dp);
-    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+    let dp = atmega_hal::Peripherals::take().unwrap();
+    let pins = atmega_hal::pins!(dp);
 
-    let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
+    let mut delay = Delay::<clock::MHz16>::new();
+
+    // set up serial interface for text output
+    let mut serial = Usart::new(
+        dp.USART0,
+        pins.pe0,
+        pins.pe1.into_output(),
+        Baudrate::<clock::MHz16>::new(57600),
+    );
+
+    let mut adc = Adc::new(dp.ADC, Default::default());
 
     let (vbg, gnd) = (
-        adc.read_blocking(&adc::channel::Vbg),
-        adc.read_blocking(&adc::channel::Gnd),
+        adc.read_blocking(&channel::Vbg),
+        adc.read_blocking(&channel::Gnd),
     );
-    ufmt::uwriteln!(&mut serial, "Vbandgap: {}", vbg).unwrap_infallible();
-    ufmt::uwriteln!(&mut serial, "Ground: {}", gnd).unwrap_infallible();
+    ufmt::uwriteln!(&mut serial, "Vbandgap: {}", vbg).unwrap();
+    ufmt::uwriteln!(&mut serial, "Ground: {}", gnd).unwrap();
 
     // To store multiple channels in an array, we use the `into_channel()` method.
-    let channels: [adc::Channel; 16] = [
-        pins.a0.into_analog_input(&mut adc).into_channel(),
-        pins.a1.into_analog_input(&mut adc).into_channel(),
-        pins.a2.into_analog_input(&mut adc).into_channel(),
-        pins.a3.into_analog_input(&mut adc).into_channel(),
-        pins.a4.into_analog_input(&mut adc).into_channel(),
-        pins.a5.into_analog_input(&mut adc).into_channel(),
-        pins.a6.into_analog_input(&mut adc).into_channel(),
-        pins.a7.into_analog_input(&mut adc).into_channel(),
-        pins.a8.into_analog_input(&mut adc).into_channel(),
-        pins.a9.into_analog_input(&mut adc).into_channel(),
-        pins.a10.into_analog_input(&mut adc).into_channel(),
-        pins.a11.into_analog_input(&mut adc).into_channel(),
-        pins.a12.into_analog_input(&mut adc).into_channel(),
-        pins.a13.into_analog_input(&mut adc).into_channel(),
-        pins.a14.into_analog_input(&mut adc).into_channel(),
-        pins.a15.into_analog_input(&mut adc).into_channel(),
+    let channels: [atmega_hal::adc::Channel; 16] = [
+        pins.pf0.into_analog_input(&mut adc).into_channel(),
+        pins.pf1.into_analog_input(&mut adc).into_channel(),
+        pins.pf2.into_analog_input(&mut adc).into_channel(),
+        pins.pf3.into_analog_input(&mut adc).into_channel(),
+        pins.pf4.into_analog_input(&mut adc).into_channel(),
+        pins.pf5.into_analog_input(&mut adc).into_channel(),
+        pins.pf6.into_analog_input(&mut adc).into_channel(),
+        pins.pf7.into_analog_input(&mut adc).into_channel(),
+        pins.pk0.into_analog_input(&mut adc).into_channel(),
+        pins.pk1.into_analog_input(&mut adc).into_channel(),
+        pins.pk2.into_analog_input(&mut adc).into_channel(),
+        pins.pk3.into_analog_input(&mut adc).into_channel(),
+        pins.pk4.into_analog_input(&mut adc).into_channel(),
+        pins.pk5.into_analog_input(&mut adc).into_channel(),
+        pins.pk6.into_analog_input(&mut adc).into_channel(),
+        pins.pk7.into_analog_input(&mut adc).into_channel(),
     ];
 
     loop {
         for (i, ch) in channels.iter().enumerate() {
             let v = adc.read_blocking(ch);
-            ufmt::uwrite!(&mut serial, "A{}: {} ", i, v).unwrap_infallible();
+            ufmt::uwrite!(&mut serial, "A{}: {} ", i, v).unwrap();
         }
 
-        ufmt::uwriteln!(&mut serial, "").unwrap_infallible();
-        arduino_hal::delay_ms(1000);
+        ufmt::uwriteln!(&mut serial, "").unwrap();
+        delay.delay_ms(1000);
     }
 }
