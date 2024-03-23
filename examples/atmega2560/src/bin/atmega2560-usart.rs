3eb4d29a8c373be301f5426efe05fd3a5a6b27f4
Armandas Jarušauskas <jarusauskas@gmail.com>
2024-03-23 11:29:04 +0900

    Copy over the mega2560 example.

---
+++ examples/atmega2560/src/bin/atmega2560-usart.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+use arduino_hal::prelude::*;
+use panic_halt as _;
+
+use embedded_hal_v0::serial::Read;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    ufmt::uwriteln!(&mut serial, "Hello from Arduino!\r").unwrap_infallible();
+
+    loop {
+        // Read a byte from the serial connection
+        let b = nb::block!(serial.read()).unwrap_infallible();
+
+        // Answer
+        ufmt::uwriteln!(&mut serial, "Got {}!\r", b).unwrap_infallible();
+    }
+}
