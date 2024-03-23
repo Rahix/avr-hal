f0336026233cf4ada34995bacabc3a7dd7d6d512
Armandas Jarušauskas <jarusauskas@gmail.com>
2024-03-23 11:29:04 +0900

    Update atmega2560-blink.rs

--- examples/atmega2560/src/bin/atmega2560-blink.rs
+++ examples/atmega2560/src/bin/atmega2560-blink.rs
@@ -2,16 +2,20 @@
 #![no_main]
 
 use panic_halt as _;
+use atmega_hal::clock;
+use atmega_hal::delay::Delay;
+use embedded_hal::delay::DelayNs;
 
-#[arduino_hal::entry]
+#[avr_device::entry]
 fn main() -> ! {
-    let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = arduino_hal::pins!(dp);
+    let dp = atmega_hal::Peripherals::take().unwrap();
+    let pins = atmega_hal::pins!(dp);
+    let mut delay = Delay::<clock::MHz16>::new();
 
-    let mut led = pins.d13.into_output().downgrade();
+    let mut led = pins.pb7.into_output();
 
     loop {
         led.toggle();
-        arduino_hal::delay_ms(1000);
+        delay.delay_ms(1000);
     }
 }
