8fcab707c04570809d02275ad00768e2b5b856d9
Armandas Jarušauskas <jarusauskas@gmail.com>
2024-03-23 11:29:04 +0900

    Update atmega2560-usart.rs

--- examples/atmega2560/src/bin/atmega2560-usart.rs
+++ examples/atmega2560/src/bin/atmega2560-usart.rs
@@ -1,24 +1,29 @@
 #![no_std]
 #![no_main]
 
-use arduino_hal::prelude::*;
+use atmega_hal::prelude::*;
+use atmega_hal::clock;
+use atmega_hal::usart::{Baudrate, Usart};
 use panic_halt as _;
 
-use embedded_hal_v0::serial::Read;
-
-#[arduino_hal::entry]
+#[avr_device::entry]
 fn main() -> ! {
-    let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = arduino_hal::pins!(dp);
-    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+    let dp = atmega_hal::Peripherals::take().unwrap();
+    let pins = atmega_hal::pins!(dp);
+    let mut serial = Usart::new(
+        dp.USART0,
+        pins.pe0,
+        pins.pe1.into_output(),
+        Baudrate::<clock::MHz16>::new(57600),
+    );
 
-    ufmt::uwriteln!(&mut serial, "Hello from Arduino!\r").unwrap_infallible();
+    ufmt::uwriteln!(&mut serial, "Hello from ATmega!\r").unwrap();
 
     loop {
         // Read a byte from the serial connection
-        let b = nb::block!(serial.read()).unwrap_infallible();
+        let b = nb::block!(serial.read()).unwrap();
 
         // Answer
-        ufmt::uwriteln!(&mut serial, "Got {}!\r", b).unwrap_infallible();
+        ufmt::uwriteln!(&mut serial, "Got {}!\r", b).unwrap();
     }
 }
