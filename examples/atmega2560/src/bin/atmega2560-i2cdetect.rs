988ec2b0524cff583a8693f6ac4254f0693fb117
Armandas Jarušauskas <jarusauskas@gmail.com>
2024-03-23 12:01:55 +0900

    Update atmega2560-i2cdetect.rs

--- examples/atmega2560/src/bin/atmega2560-i2cdetect.rs
+++ examples/atmega2560/src/bin/atmega2560-i2cdetect.rs
@@ -1,26 +1,38 @@
 #![no_std]
 #![no_main]
 
-use arduino_hal::prelude::*;
+use atmega_hal::clock;
+use atmega_hal::usart::{Baudrate, Usart};
 use panic_halt as _;
 
-#[arduino_hal::entry]
-fn main() -> ! {
-    let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = arduino_hal::pins!(dp);
-    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+type I2c = atmega_hal::i2c::I2c<clock::MHz16>;
 
-    let mut i2c = arduino_hal::I2c::new(
-        dp.TWI,
-        pins.d20.into_pull_up_input(),
-        pins.d21.into_pull_up_input(),
-        50000,
+#[avr_device::entry]
+fn main() -> ! {
+    let dp = atmega_hal::Peripherals::take().unwrap();
+    let pins = atmega_hal::pins!(dp);
+
+    // set up serial interface for text output
+    let mut serial = Usart::new(
+        dp.USART0,
+        pins.pe0,
+        pins.pe1.into_output(),
+        Baudrate::<clock::MHz16>::new(57600),
     );
 
-    ufmt::uwriteln!(&mut serial, "Write direction test:\r").unwrap_infallible();
-    i2c.i2cdetect(&mut serial, arduino_hal::i2c::Direction::Write).unwrap_infallible();
-    ufmt::uwriteln!(&mut serial, "\r\nRead direction test:\r").unwrap_infallible();
-    i2c.i2cdetect(&mut serial, arduino_hal::i2c::Direction::Read).unwrap_infallible();
+    let mut i2c = I2c::new(
+        dp.TWI,
+        pins.pd1.into_pull_up_input(),
+        pins.pd0.into_pull_up_input(),
+        50_000,
+    );
+
+    ufmt::uwriteln!(&mut serial, "Write direction test:\r").unwrap();
+    i2c.i2cdetect(&mut serial, atmega_hal::i2c::Direction::Write)
+        .unwrap();
+    ufmt::uwriteln!(&mut serial, "\r\nRead direction test:\r").unwrap();
+    i2c.i2cdetect(&mut serial, atmega_hal::i2c::Direction::Read)
+        .unwrap();
 
     loop {}
 }
