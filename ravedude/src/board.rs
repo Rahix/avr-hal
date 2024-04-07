ed7afecd52c3c7ea7aec489a6e09563e82224e4a
Colin Cai <therealcreative0708@gmail.com>
2024-04-06 23:02:20 -0400

    ravedude: add board inheritance, change baudrate syntax, add test for boards.toml

--- ravedude/src/board.rs
+++ ravedude/src/board.rs
@@ -2,12 +2,26 @@ use std::collections::HashMap;
 
 use crate::config;
 
+fn get_all_boards() -> anyhow::Result<HashMap<String, config::BoardConfig>> {
+    toml::from_str(include_str!("boards.toml")).map_err(|err| {
+        if cfg!(test) {
+            anyhow::anyhow!(
+                "boards.toml in ravedude source is invalid.\n{}",
+                err.message()
+            )
+        } else {
+            anyhow::anyhow!(
+                "boards.toml in ravedude source is invalid. This is a bug, please report it!\n{}",
+                err.message()
+            )
+        }
+    })
+}
+
 pub fn get_board(board_name: Option<&str>) -> anyhow::Result<config::BoardConfig> {
     match board_name {
         Some(board_name) => {
-            let mut all_boards: HashMap<String, config::BoardConfig> =
-                toml::from_str(include_str!("boards.toml"))
-                    .expect("boards.toml in ravedude source is invalid");
+            let mut all_boards = get_all_boards()?;
 
             all_boards.remove(board_name).ok_or_else(|| {
                 let mut msg = format!("invalid board: {board_name}\n");
@@ -24,7 +38,37 @@ pub fn get_board(board_name: Option<&str>) -> anyhow::Result<config::BoardConfig
         None => {
             let file_contents = std::fs::read_to_string("Ravedude.toml")
                 .map_err(|_| anyhow::anyhow!("no board given and couldn't find Ravedude.toml in project, either pass a board as an argument or make a Ravedude.toml."))?;
-            Ok(toml::from_str(&file_contents)?)
+
+            let mut board: config::BoardConfig = toml::from_str(&file_contents)?;
+            if let Some(inherit) = board.inherit.as_deref() {
+                let base_board = get_board(Some(inherit))?;
+                board = board.merge(base_board);
+            }
+            Ok(board)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::get_all_boards;
+
+    #[test]
+    fn validate_board_list() -> anyhow::Result<()> {
+        let all_boards = get_all_boards()?;
+
+        for board in all_boards.values() {
+            assert!(board.name.is_some());
+            assert!(board.inherit.is_none());
+            assert!(board.reset_message.is_some());
+            assert!(board.avrdude.is_some());
+            let avrdude = board.avrdude.as_ref().unwrap();
+            assert!(avrdude.programmer.is_some());
+            assert!(avrdude.partno.is_some());
+            assert!(avrdude.baudrate.is_some());
+            assert!(avrdude.do_chip_erase.is_some());
+        }
+
+        Ok(())
     }
 }
--- ravedude/src/config.rs
+++ ravedude/src/config.rs
@@ -6,18 +6,37 @@ use crate::warning;
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 #[serde(rename_all = "kebab-case")]
 pub struct BoardConfig {
-    pub name: String,
+    pub name: Option<String>,
+    pub inherit: Option<String>,
     #[serde(
         serialize_with = "serialize_reset_message",
         deserialize_with = "deserialize_reset_message",
         rename = "reset"
     )]
-    pub reset_message: Option<String>,
-    pub avrdude: BoardAvrdudeOptions,
+    pub reset_message: Option<Option<String>>,
+    pub avrdude: Option<BoardAvrdudeOptions>,
     pub usb_info: Option<BoardUSBInfo>,
 
     #[serde(flatten)]
     pub overrides: BoardOverrides,
+}
+
+impl BoardConfig {
+    pub fn merge(self, base: BoardConfig) -> Self {
+        Self {
+            name: self.name.or(base.name),
+            // inherit is used to decide what BoardConfig to inherit and isn't used anywhere else
+            inherit: None,
+            reset_message: self.reset_message.or(base.reset_message),
+            avrdude: self
+                .avrdude
+                .and_then(|avrdude| base.avrdude.map(|base_avrdude| avrdude.merge(base_avrdude))),
+            usb_info: self.usb_info.or(base.usb_info),
+
+            // overrides aren't related to the board
+            overrides: self.overrides,
+        }
+    }
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
@@ -26,23 +45,28 @@ struct ResetOptions {
     message: Option<String>,
 }
 
-fn serialize_reset_message<S>(val: &Option<String>, serializer: S) -> Result<S::Ok, S::Error>
+fn serialize_reset_message<S>(
+    val: &Option<Option<String>>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
 where
     S: serde::Serializer,
 {
-    let reset_options = ResetOptions {
+    let reset_options = val.as_ref().map(|val| ResetOptions {
         automatic: val.is_none(),
         message: val.clone(),
-    };
+    });
 
     reset_options.serialize(serializer)
 }
 
-fn deserialize_reset_message<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
+fn deserialize_reset_message<'de, D>(deserializer: D) -> Result<Option<Option<String>>, D::Error>
 where
     D: serde::Deserializer<'de>,
 {
-    let reset_options = ResetOptions::deserialize(deserializer)?;
+    let Some(reset_options) = Option::<ResetOptions>::deserialize(deserializer)? else {
+        return Ok(None);
+    };
 
     if reset_options.automatic && reset_options.message.is_some() {
         return Err(serde::de::Error::custom(
@@ -55,16 +79,55 @@ where
         ));
     }
 
-    Ok(reset_options.message)
+    Ok(Some(reset_options.message))
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 #[serde(rename_all = "kebab-case")]
 pub struct BoardAvrdudeOptions {
-    pub programmer: String,
-    pub partno: String,
-    pub baudrate: Option<NonZeroU32>,
-    pub do_chip_erase: bool,
+    pub programmer: Option<String>,
+    pub partno: Option<String>,
+    #[serde(
+        serialize_with = "serialize_baudrate",
+        deserialize_with = "deserialize_baudrate"
+    )]
+    pub baudrate: Option<Option<NonZeroU32>>,
+    pub do_chip_erase: Option<bool>,
+}
+impl BoardAvrdudeOptions {
+    pub fn merge(self, base: Self) -> Self {
+        Self {
+            programmer: self.programmer.or(base.programmer),
+            partno: self.partno.or(base.partno),
+            baudrate: self.baudrate.or(base.baudrate),
+            do_chip_erase: self.do_chip_erase.or(base.do_chip_erase),
+        }
+    }
+}
+fn serialize_baudrate<S>(val: &Option<Option<NonZeroU32>>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    let baudrate = val.as_ref().map(|val| val.map_or(-1, |x| x.get() as i32));
+
+    baudrate.serialize(serializer)
+}
+
+fn deserialize_baudrate<'de, D>(deserializer: D) -> Result<Option<Option<NonZeroU32>>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let Some(baudrate) = Option::<i32>::deserialize(deserializer)? else {
+        return Ok(None);
+    };
+
+    match NonZeroU32::new(baudrate as _) {
+        None if baudrate == -1 => Ok(Some(None)),
+        Some(nonzero_baudrate) => Ok(Some(Some(nonzero_baudrate))),
+        _ => Err(serde::de::Error::custom(format!(
+            "invalid baudrate: {baudrate}"
+        ))),
+    }
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
