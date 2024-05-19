3586a7e2a2260c3b1597280c3f305ff46f887de3
tippfehlr <tippfehlr@tippfehlr.eu>
2024-05-19 22:43:20 +0200

    ravedude: add output_mode, newline_after and newline_on
    sometimes printing ascii isn’t useful, and the raw hex, binary or
    decimal data should be printed.
    
    newline_after adds a newline after the n’th byte
    newline_on adds a newline after the given byte

--- ravedude/src/console.rs
+++ ravedude/src/console.rs
@@ -1,8 +1,35 @@
-use anyhow::Context as _;
 use std::io::Read as _;
 use std::io::Write as _;
 
-pub fn open(port: &std::path::Path, baudrate: u32) -> anyhow::Result<()> {
+use anyhow::Context as _;
+use colored::Colorize as _;
+
+use crate::config::OutputMode;
+use crate::config::OutputMode::*;
+use crate::task_message;
+
+pub fn open(
+    port: &std::path::PathBuf,
+    baudrate: u32,
+    output_mode: Option<OutputMode>,
+    newline_on: Option<char>,
+    newline_after: Option<u8>,
+) -> anyhow::Result<()> {
+    task_message!("Console", "{} at {} baud", port.display(), baudrate);
+    task_message!(
+        "Output",
+        "{}",
+        match output_mode {
+            Some(Ascii) | None => "ascii",
+            Some(Hex) => "hexadecimal",
+            Some(Dec) => "decimal",
+            Some(Bin) => "binary",
+        }
+    );
+    task_message!("", "{}", "CTRL+C to exit.".dimmed());
+    // Empty line for visual consistency
+    eprintln!();
+
     let mut rx = serialport::new(port.to_string_lossy(), baudrate)
         .timeout(std::time::Duration::from_secs(2))
         .open_native()
@@ -14,11 +41,28 @@ pub fn open(port: &std::path::Path, baudrate: u32) -> anyhow::Result<()> {
 
     // Set a CTRL+C handler to terminate cleanly instead of with an error.
     ctrlc::set_handler(move || {
-        eprintln!("");
+        eprintln!();
         eprintln!("Exiting.");
         std::process::exit(0);
     })
     .context("failed setting a CTRL+C handler")?;
+
+    let newline_after = match newline_after {
+        Some(n) => n,
+        None => match output_mode {
+            Some(Hex) | Some(Dec) => 16,
+            Some(Bin) => 8,
+            _ => 0,
+        },
+    };
+
+    let (spaces, space_after) = if newline_on.is_none() && newline_after % 4 == 0 {
+        (true, 4)
+    } else {
+        (false, 0)
+    };
+
+    let mut byte_count = 0;
 
     // Spawn a thread for the receiving end because stdio is not portably non-blocking...
     std::thread::spawn(move || loop {
@@ -41,7 +85,38 @@ pub fn open(port: &std::path::Path, baudrate: u32) -> anyhow::Result<()> {
                         }
                     }
                 }
-                stdout.write(&buf[..count]).unwrap();
+                match output_mode {
+                    Some(Ascii) | None => {
+                        stdout.write_all(&buf).unwrap();
+                    }
+                    _ => {
+                        for byte in &buf[..count] {
+                            byte_count += 1;
+                            match output_mode {
+                                Some(Ascii) | None => unreachable!(),
+                                Some(Hex) => {
+                                    write!(stdout, "{:02x} ", byte).unwrap();
+                                }
+                                Some(Dec) => {
+                                    write!(stdout, "{:03} ", byte).unwrap();
+                                }
+                                Some(Bin) => {
+                                    write!(stdout, "{:08b} ", byte).unwrap();
+                                }
+                            }
+                            // don’t execute in ascii mode, ascii is unreachable here
+                            if spaces && byte_count % space_after == 0 {
+                                write!(stdout, " ").unwrap();
+                            }
+                            if newline_on.is_none() && byte_count % newline_after == 0 {
+                                writeln!(stdout).unwrap();
+                            }
+                            if newline_on.is_some() && *byte as char == newline_on.unwrap() {
+                                writeln!(stdout).unwrap();
+                            }
+                        }
+                    }
+                }
                 stdout.flush().unwrap();
             }
             Err(e) => {
