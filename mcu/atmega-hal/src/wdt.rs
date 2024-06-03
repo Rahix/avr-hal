67bc7f9f9080e52f17f1e6c0b8c471204e65531e
jonas <@DISTRIB_ID=Pop.com>
2024-06-03 13:23:45 +0200

    impl wdt modes

--- mcu/atmega-hal/src/wdt.rs
+++ mcu/atmega-hal/src/wdt.rs
@@ -1,5 +1,5 @@
 #[allow(unused_imports)]
-pub use avr_hal_generic::wdt::{Timeout, WdtOps};
+pub use avr_hal_generic::wdt::{Timeout, WdtMode, WdtOps};
 
 pub type Wdt = avr_hal_generic::wdt::Wdt<crate::Atmega, crate::pac::WDT>;
 
