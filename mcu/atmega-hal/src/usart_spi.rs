f4052df82f59ef7d630c2118ef81d56720c763f7
CoolSlimbo <lrabie07@outlook.com>
2024-06-23 17:18:00 +1000

    Atmega1280 and 2560 UsartSpi Implimentations

--- mcu/atmega-hal/src/usart_spi.rs
+++ mcu/atmega-hal/src/usart_spi.rs
@@ -39,3 +39,63 @@ avr_hal_generic::impl_usart_spi! {
     miso: port::PE0,
     cs: port::Dynamic,
 }
+
+#[cfg(any(feature = "atmega1280", feature = "atmega2560"))]
+pub type Usart1Spi = avr_hal_generic::usart_spi::UsartSpi<
+    crate::Atmega,
+    crate::pac::USART1,
+    port::PD5,
+    port::PD3,
+    port::PD2,
+    port::Dynamic,
+>;
+#[cfg(any(feature = "atmega1280", feature = "atmega2560"))]
+avr_hal_generic::impl_usart_spi! {
+    hal: crate::Atmega,
+    peripheral: crate::pac::USART1,
+    register_suffix: 1,
+    sclk: port::PD5,
+    mosi: port::PD3,
+    miso: port::PD2,
+    cs: port::Dynamic,
+}
+
+#[cfg(any(feature = "atmega1280", feature = "atmega2560"))]
+pub type Usart2Spi = avr_hal_generic::usart_spi::UsartSpi<
+    crate::Atmega,
+    crate::pac::USART2,
+    port::PH2,
+    port::PH1,
+    port::PH0,
+    port::Dynamic,
+>;
+#[cfg(any(feature = "atmega1280", feature = "atmega2560"))]
+avr_hal_generic::impl_usart_spi! {
+    hal: crate::Atmega,
+    peripheral: crate::pac::USART2,
+    register_suffix: 2,
+    sclk: port::PH2,
+    mosi: port::PH1,
+    miso: port::PH0,
+    cs: port::Dynamic,
+}
+
+#[cfg(any(feature = "atmega1280", feature = "atmega2560"))]
+pub type Usart3Spi = avr_hal_generic::usart_spi::UsartSpi<
+    crate::Atmega,
+    crate::pac::USART3,
+    port::PJ2,
+    port::PJ1,
+    port::PJ0,
+    port::Dynamic,
+>;
+#[cfg(any(feature = "atmega1280", feature = "atmega2560"))]
+avr_hal_generic::impl_usart_spi! {
+    hal: crate::Atmega,
+    peripheral: crate::pac::USART3,
+    register_suffix: 3,
+    sclk: port::PJ2,
+    mosi: port::PJ1,
+    miso: port::PJ0,
+    cs: port::Dynamic,
+}
