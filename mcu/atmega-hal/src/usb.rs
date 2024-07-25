8d0f94eb6cc8a4dcb10ab66e414c6df61739f0f1
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2024-07-25 08:14:29 -0400

    Switch to using pac for writing less code

--- mcu/atmega-hal/src/usb.rs
+++ mcu/atmega-hal/src/usb.rs
@@ -1,9 +1,10 @@
 
 pub use avr_hal_generic::usb::*;
 
-#[cfg(feature = "atmega32u4")]
-use avr_device::atmega32u4::USB_DEVICE;
+#[cfg(any(feature = "atmega32u4", feature = "atmega8u2"))]
+use crate::pac::USB_DEVICE;
 
+// use crate::pac::USB0 as USB_DEVICE;
 
 avr_hal_generic::usb::create_usb_bus! {
 	USB_DEVICE,
