2f1509ce3e9360345add66ac03c17a5e178b7bba
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2024-08-02 22:43:19 -0400

    A better error handler

--- examples/arduino-leonardo/src/bin/std_stub/mod.rs
+++ examples/arduino-leonardo/src/bin/std_stub/mod.rs
@@ -1,7 +1,9 @@
+#![feature(panic_info_message)]
 //! Replacement for avr-std-stub with a custom panic handler.
 
 use core::panic::PanicInfo;
 
+use arduino_hal::prelude::*;
 use arduino_hal::{delay_ms, pins, Peripherals};
 
 #[panic_handler]
@@ -9,6 +11,19 @@ fn panic(_info: &PanicInfo) -> ! {
     let dp = unsafe { Peripherals::steal() };
     let pins = pins!(dp);
     let mut status = pins.d13.into_output();
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    ufmt::uwriteln!(&mut serial, "I panicked because {}!\r", _info.message().as_str().unwrap_or("useless")).unwrap_infallible();
+    ufmt::uwriteln!(&mut serial, "I panicked at {}!\r", _info.location().unwrap().file()).unwrap_infallible();
+
+    delay_ms(100);
+
+    ufmt::uwriteln!(&mut serial, "I panicked on {}!\r", _info.location().unwrap().line()).unwrap_infallible();
+
+    if let Some(s) = _info.payload().downcast_ref::<&str>() {
+        ufmt::uwriteln!(&mut serial, "More info: {}!\r", s).unwrap_infallible();
+    } 
+
     loop {
         status.set_high();
         delay_ms(100);
