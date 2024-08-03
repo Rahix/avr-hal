b504d117468b9b52dbd3c1db69114a004c70efdb
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2024-08-03 00:07:31 -0400

    Even better panic handling

--- examples/arduino-leonardo/src/bin/std_stub/mod.rs
+++ examples/arduino-leonardo/src/bin/std_stub/mod.rs
@@ -1,28 +1,48 @@
 #![feature(panic_info_message)]
 //! Replacement for avr-std-stub with a custom panic handler.
 
+use core::fmt::{self, Debug};
 use core::panic::PanicInfo;
 
-use arduino_hal::prelude::*;
+use arduino_hal::hal::port::{PD2, PD3};
+use arduino_hal::hal::Atmega;
+use arduino_hal::pac::USART1;
+use arduino_hal::port::mode::{Input, Output};
+use arduino_hal::port::Pin;
+use arduino_hal::{prelude::*, Usart};
 use arduino_hal::{delay_ms, pins, Peripherals};
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    use ::core::fmt::Write as _;
+
     let dp = unsafe { Peripherals::steal() };
     let pins = pins!(dp);
     let mut status = pins.d13.into_output();
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    ufmt::uwriteln!(&mut serial, "I panicked because {}!\r", _info.message().as_str().unwrap_or("useless")).unwrap_infallible();
-    ufmt::uwriteln!(&mut serial, "I panicked at {}!\r", _info.location().unwrap().file()).unwrap_infallible();
+    struct UartWriter {
+        uart: Usart<USART1, Pin<Input, PD2>, Pin<Output, PD3>>
+    }
+    impl ::core::fmt::Write for UartWriter {
+        fn write_str(&mut self, s: &str) -> ::core::fmt::Result {
+            ufmt::uwriteln!(&mut self.uart, "{}", s).unwrap_infallible();
+            Ok(())
+        }
+    }
 
-    delay_ms(100);
+    // ufmt::uwriteln!(&mut serial, "I panicked at {}!\r", _info.location().unwrap().file()).unwrap_infallible();
 
-    ufmt::uwriteln!(&mut serial, "I panicked on {}!\r", _info.location().unwrap().line()).unwrap_infallible();
+    // delay_ms(100);
 
-    if let Some(s) = _info.payload().downcast_ref::<&str>() {
-        ufmt::uwriteln!(&mut serial, "More info: {}!\r", s).unwrap_infallible();
-    } 
+    // ufmt::uwriteln!(&mut serial, "I panicked on {}!\r", _info.location().unwrap().line()).unwrap_infallible();
+
+    // if let Some(s) = _info.payload().downcast_ref::<&str>() {
+    //     ufmt::uwriteln!(&mut serial, "More info: {}!\r", s).unwrap_infallible();
+    // } 
+
+    let mut uart = UartWriter { uart: serial };
+    ::core::writeln!(uart, "{}", _info).ok();
 
     loop {
         status.set_high();
