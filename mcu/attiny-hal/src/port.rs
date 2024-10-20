15080aa41e726bc720adf36ce0fc5a030390c822
Armandas Jarušauskas <jarusauskas@gmail.com>
2024-10-20 18:54:55 +0900

    attiny-hal: Add examples to documentation

--- mcu/attiny-hal/src/port.rs
+++ mcu/attiny-hal/src/port.rs
@@ -1,3 +1,22 @@
+//! Port
+//!
+//! # Example
+//!
+//! For full source code, please refer to the ATmega port example:
+//! [`atmega2560-blink.rs`](https://github.com/Rahix/avr-hal/blob/main/examples/atmega2560/src/bin/atmega2560-blink.rs)
+//!
+//! ```
+//! let dp = attiny_hal::Peripherals::take().unwrap();
+//! let pins = attiny_hal::pins!(dp);
+//!
+//! let mut led = pins.pb2.into_output();
+//!
+//! loop {
+//!     led.toggle();
+//!     delay_ms(1000);
+//! }
+//! ```
+
 pub use avr_hal_generic::port::{mode, PinMode, PinOps};
 
 #[cfg(feature = "attiny2313")]
