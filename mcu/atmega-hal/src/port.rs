5e476717e139a1704f4b3753467136f27031d8ff
andber1 <82754113+andber1@users.noreply.github.com>
2025-04-01 21:52:42 +0200

    atmega-hal: Add ATmega16 support

--- mcu/atmega-hal/src/port.rs
+++ mcu/atmega-hal/src/port.rs
@@ -33,7 +33,7 @@ avr_hal_generic::impl_port_traditional! {
     }
 }
 
-#[cfg(any(feature = "atmega164pa"))]
+#[cfg(any(feature = "atmega16", feature = "atmega164pa"))]
 avr_hal_generic::impl_port_traditional! {
     enum Ports {
         A: crate::pac::PORTA = [0, 1, 2, 3, 4, 5, 6 ,7],
